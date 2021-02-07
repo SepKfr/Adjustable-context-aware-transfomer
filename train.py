bf5e9d0c7a01b3a609abb594917583ba271b8a75
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-02-07 14:52:39 -0500

    uncommenting

--- train.py
+++ train.py
@@ -170,7 +170,7 @@ def main():
                       rnn_type="gru",
                       d_r=0.1)
 
-    run(gru_conv, "GruConv")
+    run(gru_conv, "GruConv")'''
 
     lstm = RNN(n_layers=n_layers,
                hidden_size=d_model,
@@ -188,7 +188,7 @@ def main():
               rnn_type="GRU",
               d_r=0.1)
 
-    run(gru, "gru")'''
+    run(gru, "gru")
 
     if os.path.exists("erros.json"):
         with open("erros.json") as json_file:
