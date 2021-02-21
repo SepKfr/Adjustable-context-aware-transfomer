3bd19fd82663726e01cfed29f8af5c52e6138d1a
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-02-21 15:30:07 -0500

    solve the dim mismatch problem

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ inputs = pickle.load(open("inputs.p", "rb"))
 outputs = pickle.load(open("outputs.p", "rb"))
 scalers = pickle.load(open("scalers.pkl", "rb"))
 
-max_len = min(len(inputs), 200)
+max_len = min(len(inputs), 1000)
 inputs = inputs[-max_len:, :, :]
 outputs = outputs[-max_len:, :]
 trn_len = int(inputs.shape[0] * 0.95)
