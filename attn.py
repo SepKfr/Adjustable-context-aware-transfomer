1b5d5a64e0ec9038ef1860ca530b6a117bc1f79c
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-11 09:40:09 -0500

    con attn

--- attn.py
+++ attn.py
@@ -234,7 +234,7 @@ class Encoder(nn.Module):
 
     def forward(self, x):
 
-        '''if self.attn_type == 'con_conv':
+        if self.attn_type == 'con_conv':
             enc_outputs = self.src_emb_conv(x.permute(0, 2, 1))
             enc_outputs = enc_outputs.permute(0, 2, 1)
             enc_outputs = self.pos_emb(enc_outputs)
@@ -244,18 +244,19 @@ class Encoder(nn.Module):
             enc_outputs = self.src_emb(x)
             enc_outputs = self.pos_emb(enc_outputs)
             padding = int(self.kernel_size / 2)
-            key = F.pad(enc_outputs.permute(0, 2, 1), pad=(padding, padding, 0, 0))
+            '''key = F.pad(enc_outputs.permute(0, 2, 1), pad=(padding, padding, 0, 0))
             key = key.permute(0, 2, 1)
             value = F.pad(enc_outputs.permute(0, 2, 1), pad=(padding, padding, 0, 0))
-            value = value.permute(0, 2, 1)
+            value = value.permute(0, 2, 1)'''
             mask = get_con_mask(enc_outputs, enc_outputs, padding).to(self.device)
             for _ in range(self.n_layers):
-                enc_outputs, _ = self.src_emb_attn(enc_outputs, enc_outputs, enc_outputs, mask)'''
+                enc_outputs, _ = self.src_emb_attn(enc_outputs, enc_outputs, enc_outputs, mask)
 
-        enc_outputs = self.src_emb(x)
+        else:
+            enc_outputs = self.src_emb(x)
 
-        if self.pe == 'sincos':
-            enc_outputs = self.pos_emb(enc_outputs)
+        '''if self.pe == 'sincos':
+            enc_outputs = self.pos_emb(enc_outputs)'''
 
         if not self.local:
             enc_self_attn_mask = None
@@ -332,7 +333,7 @@ class Decoder(nn.Module):
 
     def forward(self, dec_inputs, enc_inputs, enc_outputs, training=True):
 
-        '''if self.attn_type == "con_conv":
+        if self.attn_type == "con_conv":
             dec_outputs = self.tgt_emb_conv(dec_inputs.permute(0, 2, 1))
             dec_outputs = dec_outputs.permute(0, 2, 1)
             dec_outputs = self.pos_emb(dec_outputs)
@@ -341,6 +342,7 @@ class Decoder(nn.Module):
 
             x = self.tgt_emb(dec_inputs)
             dec_outputs = self.pos_emb(x)
+            dec_outputs = self.pos_emb(dec_outputs)
             padding = int(self.kernel_size / 2)
             key = F.pad(dec_outputs.permute(0, 2, 1), pad=(padding, padding, 0, 0))
             key = key.permute(0, 2, 1)
@@ -348,12 +350,13 @@ class Decoder(nn.Module):
             value = value.permute(0, 2, 1)
             mask = get_con_mask(dec_outputs, key, padding)
             for _ in range(self.n_layers):
-                dec_outputs, _ = self.tgt_emb_attn(x, key, value, mask)'''
+                dec_outputs, _ = self.tgt_emb_attn(x, key, value, mask)
 
-        dec_outputs = self.tgt_emb(dec_inputs)
+        else:
+            dec_outputs = self.tgt_emb(dec_inputs)
 
-        if self.pe == 'sincos':
-            dec_outputs = self.pos_emb(dec_outputs)
+        '''if self.pe == 'sincos':
+            dec_outputs = self.pos_emb(dec_outputs)'''
 
         dec_self_attn_mask = get_attn_subsequent_mask(dec_inputs)
 
