7c172c801babeec5fd1cfd954fd5e9fdbb6607b9
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-11 11:41:45 -0500

    trying continues convolution to compare with attention continues

--- train.py
+++ train.py
@@ -187,7 +187,7 @@ def main():
     call_atn_model('attn_con', 'sincos', 'con_attn', False, 0, x_en, x_de, x_en_t,
                    x_de_t, y_true, y_true_t, params)
 
-    call_atn_model('attn_con', 'sincos', 'con_conv', False, 0, x_en, x_de, x_en_t,
+    call_atn_model('attn_con_conv', 'sincos', 'con_conv', False, 0, x_en, x_de, x_en_t,
                    x_de_t, y_true, y_true_t, params)
 
     call_atn_model('attn_con_gl', 'sincos', 'con_attn', True, params.loc_seq_len, x_en, x_de,
