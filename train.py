c5be1c6444bade70142849a31ed3955bbcd0546b
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-14 10:30:31 -0400

    run rnn on jelly

--- train.py
+++ train.py
@@ -185,7 +185,7 @@ def main():
     x_de_t = test_x[:, -seq_len:, :]
     y_true_t = test_y[:, :, :]
 
-    call_atn_model('attn', 'sincos', 'attn', False, 0, x_en, x_de, x_en_t,
+    '''call_atn_model('attn', 'sincos', 'attn', False, 0, x_en, x_de, x_en_t,
                    x_de_t, y_true, y_true_t, params)
 
     call_atn_model('attn_con', 'sincos', 'con_attn', False, 0, x_en, x_de, x_en_t,
@@ -201,9 +201,9 @@ def main():
                    x_en_t, x_de_t, y_true, y_true_t, params)
 
     call_atn_model('attn_con_conv_gl', 'sincos', 'attn', True, params.loc_seq_len, x_en, x_de,
-                   x_en_t, x_de_t, y_true, y_true_t, params)
+                   x_en_t, x_de_t, y_true, y_true_t, params)'''
 
-    '''cnn = CNN(input_size=input_size,
+    cnn = CNN(input_size=input_size,
               output_size=output_size,
               out_channel=d_model,
               kernel=kernel,
@@ -224,9 +224,9 @@ def main():
         lstm = nn.DataParallel(lstm)
     lstm.to(device)
 
-    run(lstm, "lstm", [x_en, x_de], [x_en_t, x_de_t], y_true, y_true_t, params)'''
+    run(lstm, "lstm", [x_en, x_de], [x_en_t, x_de_t], y_true, y_true_t, params)
 
-    '''gru = RNN(n_layers=n_layers,
+    gru = RNN(n_layers=n_layers,
               hidden_size=64,
               input_size=input_size,
               output_size=output_size,
@@ -236,7 +236,7 @@ def main():
         gru = nn.DataParallel(gru)
     gru.to(device)
 
-    run(gru, "gru", [x_en, x_de], [x_en_t, x_de_t], y_true, y_true_t, params)'''
+    run(gru, "gru", [x_en, x_de], [x_en_t, x_de_t], y_true, y_true_t, params)
 
     if os.path.exists("erros.json"):
         with open("erros.json") as json_file:
