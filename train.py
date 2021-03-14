d65b25915a2b83815ec547d505535955f2e9b970
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-14 12:56:51 -0400

    running with warm up

--- train.py
+++ train.py
@@ -111,7 +111,7 @@ def train(model, trn_x, y_t, batch_size, name, run_num, site):
 
     x_en, x_de, y_t = batching(batch_size, trn_x[0], trn_x[1], y_t)
 
-    optimizer = Adam(model.parameters(), lr=0.0001, weight_decay=0.0005)
+    optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0005)
     criterion = nn.MSELoss()
     model.train()
     num_steps = len(trn_x) * n_ephocs
@@ -125,6 +125,8 @@ def train(model, trn_x, y_t, batch_size, name, run_num, site):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            lr_scheduler.step()
+            warmup_scheduler.dampen()
 
     path = "models_{}_{}".format(site, y_t.shape[2])
     if not os.path.exists(path):
