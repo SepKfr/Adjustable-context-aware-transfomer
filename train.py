d1a95d4eb53c48da809eec20f19bd74339400c64
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-26 13:56:56 -0400

    normalized data

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def inverse_transform(data):
 
 def evaluate(model, tst_x, y_t):
 
-    y_t_in = inverse_transform(y_t)
+    #y_t_in = inverse_transform(y_t)
     b, seq_len, f = y_t.shape
 
     model.eval()
@@ -84,8 +84,9 @@ def evaluate(model, tst_x, y_t):
 
         otps = model(tst_x[0].to(device), tst_x[1].to(device), training=False)
 
-    otps_in = inverse_transform(otps)
-    metrics = Metrics(otps_in.view(seq_len * b * f), y_t_in.view(seq_len * b * f))
+
+    #otps_in = inverse_transform(otps)
+    metrics = Metrics(otps.view(seq_len * b * f), y_t.view(seq_len * b * f))
     return metrics.rmse, metrics.mae
 
 
