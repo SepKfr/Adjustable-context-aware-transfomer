ceeef416bb000042b5c3c72f30bdc0728571aded
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-26 10:33:02 -0400

    moving

--- train.py
+++ train.py
@@ -199,12 +199,12 @@ def main():
 
     if params.server == 'c01':
 
-        call_atn_model('attn_hist', 'sincos', 'attn',
+        call_atn_model('attn_con_hist', 'sincos', 'con',
                        seq_len, x_en, x_de, x_en_t,
                        x_de_t, y_true,
                        y_true_t, params.seq_len_pred, params)
 
-        call_atn_model('attn_con_hist', 'sincos', 'con',
+        call_atn_model('attn_hist', 'sincos', 'attn',
                        seq_len, x_en, x_de, x_en_t,
                        x_de_t, y_true,
                        y_true_t, params.seq_len_pred, params)
