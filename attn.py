e1b575c385eeef9e2e75490c673a60f8ecc342cd
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-28 14:50:44 -0400

    fix cutoff

--- attn.py
+++ attn.py
@@ -43,6 +43,7 @@ def get_con_mask(seq_q, seq_k, padding):
 def get_con_vecs(seq, cutoff):
 
     batch_size, n_h, seq_len, d_k = seq.shape
+    cutoff = seq_len if seq_len < cutoff else cutoff
     seq = seq.reshape(batch_size, seq_len, n_h * d_k)
 
     up_t = seq.unsqueeze(1).repeat(1, seq_len, 1, 1).permute(0, 3, 1, 2)
