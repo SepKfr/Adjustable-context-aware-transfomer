ba6d1bbfefdc13dabcb09c3213158b4886c45f48
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-29 16:48:30 -0400

    trying some changes

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ def evaluate(model, tst_x, y_t):
         otps = model(tst_x[0].to(device), tst_x[1].to(device), training=False)
 
     #otps_in = inverse_transform(otps)
-    metrics = Metrics(otps.view(seq_len * b * f), y_t.view(seq_len * b * f))
+    metrics = Metrics(otps.view(seq_len * b * f), y_t.to(device).view(seq_len * b * f))
 
     return metrics.rmse, metrics.mae, otps
 
@@ -111,7 +111,6 @@ def train(model, trn_x, y_t, batch_size):
             optimizer.step()
             lr_scheduler.step()
             warmup_scheduler.dampen()
-        print(total_loss)
 
 
 def run(model, name, trn_x, trn_y, params):
