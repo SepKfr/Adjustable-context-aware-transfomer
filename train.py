1a011ed839affcd68137e7323e5dfdcd746a85a2
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-30 18:19:09 -0400

    de normalized validation set

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ x_en, x_de, y_true = batching(params.batch_size, train_x[:, :-seq_len, :],
 x_en_t, x_de_t, y_true_t = x_en[-1, :, :, :], x_de[-1, :, :, :], y_true[-1, :, :, :]
 x_en_v, x_de_v, y_true_v = x_en[-2:-1, :, :, :], x_de[-2:-1, :, :, :], y_true[-2:-1, :, :, :]
 x_en, x_de, y_true = x_en[:-2, :, :, :], x_de[:-2, :, :, :], y_true[:-2, :, :, :]
-print(x_en_v.shape)
+
 
 erros = dict()
 
