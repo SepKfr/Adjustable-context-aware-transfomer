da5d78333e1ce6ac917ce18e0a5ae55b171cd448
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-31 14:42:19 -0400

    change atten cont

--- attn.py
+++ attn.py
@@ -111,7 +111,8 @@ class ScaledDotProductAttention(nn.Module):
         if self.attn_type == "con":
             Q = get_con_vecs(Q, self.cutoff).to(self.device)
             K = get_con_vecs(K, self.cutoff).to(self.device)
-            scores = torch.einsum('bhqcd,bhkld->bhqk', Q, K) / np.sqrt(self.d_k)
+            V = K
+            scores = torch.einsum('bhqcd,bhkld->bhqkc', Q, K) / np.sqrt(self.d_k)
 
         else:
             scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
@@ -123,7 +124,8 @@ class ScaledDotProductAttention(nn.Module):
 
         attn = nn.Softmax(dim=-1)(scores)
         if self.attn_type == "con":
-            context = torch.matmul(attn, V)
+            context = torch.einsum('bhqkc,bhvcd->bhvd', attn, V)
+            attn = torch.einsum('bhqkc->bhqk', attn)
         else:
             context = torch.matmul(attn, V)
         return context, attn
