03a1c77bd5698165e6e62dcd9602508b803dec1a
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-03-31 16:18:01 -0400

    inverse transform

--- utils.py
+++ utils.py
@@ -16,15 +16,16 @@ class Metrics:
 
 def inverse_transform(data):
 
-    b, n, d, hw = data.shape
+    b_n, n, d, hw = data.shape
     inv_data = torch.zeros(data.shape)
 
-    for i, scalers_per_site in enumerate(scalers):
-        f, scaler = list(scalers_per_site.scalers.items())[1]
-        dat = data[:, :, :, 0]
-        dat = dat.view(b*n*d)
-        in_dat = scaler.inverse_transform(dat.cpu().detach().numpy().reshape(-1, 1))
-        in_dat = torch.from_numpy(np.array(in_dat).flatten())
-        inv_data[:, :, :, 0] = in_dat.view(n, d)
+    for b in b_n:
+        for i, scalers_per_site in enumerate(scalers):
+            f, scaler = list(scalers_per_site.scalers.items())[1]
+            dat = data[b, :, :, 0]
+            dat = dat.view(n*d)
+            in_dat = scaler.inverse_transform(dat.cpu().detach().numpy().reshape(-1, 1))
+            in_dat = torch.from_numpy(np.array(in_dat).flatten())
+            inv_data[b, :, :, 0] = in_dat.view(n, d)
 
     return inv_data
