c396d20f9eac08066d4259478be694d7feec7f4e
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-02 15:49:20 -0400

    grid search again!

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ import json
 import os
 import pytorch_warmup as warmup
 from clearml import Task, Logger
+from utils import inverse_transform
 
 
 def batching(batch_size, x_en, x_de, y_t):
@@ -182,7 +183,9 @@ def main():
     test_loss = 0
     for j in range(test_en.shape[0]):
         output = model(test_en[j].to(device), test_de[j].to(device), training=True)
-        loss = criterion(test_y[j].to(device), output)
+        output = inverse_transform(output).to(device)
+        y_true = inverse_transform(test_y[j]).to(device)
+        loss = criterion(y_true, output)
         test_loss += loss.item()
 
     erros[args.name] = list()
