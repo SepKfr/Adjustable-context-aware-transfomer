94187e4e2d6d22d734ab4f2393b8ce40b36114e0
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-03 17:57:21 -0400

    denormalize

--- train.py
+++ train.py
@@ -103,12 +103,16 @@ def main():
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--cutoff", type=int, default=16)
     parser.add_argument("--d_model", type=int, default=[32, 64])
+    parser.add_argument("--d_model_best", type=int)
     parser.add_argument("--dff", type=int, default=64)
     parser.add_argument("--n_heads", type=list, default=[1, 4])
+    parser.add_argument("--n_heads_best", type=int)
     parser.add_argument("--n_layers", type=list, default=[1, 3])
+    parser.add_argument("--n_layers_best", type=int)
     parser.add_argument("--kernel", type=int, default=1)
     parser.add_argument("--out_channel", type=int, default=32)
     parser.add_argument("--dr", type=list, default=[0.1, 0.5])
+    parser.add_argument("--dr_best", type=float)
     parser.add_argument("--lr", type=list, default=0.0001)
     parser.add_argument("--n_epochs", type=int, default=5)
     parser.add_argument("--run_num", type=int, default=1)
@@ -117,6 +121,7 @@ def main():
     parser.add_argument("--name", type=str, default='attn')
     parser.add_argument("--site", type=str, default="WHB")
     parser.add_argument("--server", type=str, default="c01")
+    parser.add_argument("--training", type=bool, default=True)
     args = parser.parse_args()
     #args = task.connect(args)
 
@@ -144,32 +149,36 @@ def main():
 
     criterion = nn.MSELoss()
 
-    val_loss = 1e5
-    best_config = None
-    for layers in args.n_layers:
-        for heads in args.n_heads:
-            for d_model in args.d_model:
-                for dr in args.dr:
-                    d_k = int(d_model / heads)
-                    model = Attn(src_input_size=train_en.shape[3],
-                                 tgt_input_size=train_y.shape[3],
-                                 d_model=d_model,
-                                 d_ff=d_model*2,
-                                 d_k=d_k, d_v=d_k, n_heads=heads,
-                                 n_layers=layers, src_pad_index=0,
-                                 tgt_pad_index=0, device=device,
-                                 pe=args.pos_enc, attn_type=args.attn_type,
-                                 seq_len=seq_len, seq_len_pred=args.seq_len_pred,
-                                 cutoff=args.cutoff, dr=dr).to(device)
-                    config = layers, heads, d_model, dr
+    if args.training:
+        val_loss = 1e5
+        best_config = None
+        for layers in args.n_layers:
+            for heads in args.n_heads:
+                for d_model in args.d_model:
+                    for dr in args.dr:
+                        d_k = int(d_model / heads)
+                        model = Attn(src_input_size=train_en.shape[3],
+                                     tgt_input_size=train_y.shape[3],
+                                     d_model=d_model,
+                                     d_ff=d_model*2,
+                                     d_k=d_k, d_v=d_k, n_heads=heads,
+                                     n_layers=layers, src_pad_index=0,
+                                     tgt_pad_index=0, device=device,
+                                     pe=args.pos_enc, attn_type=args.attn_type,
+                                     seq_len=seq_len, seq_len_pred=args.seq_len_pred,
+                                     cutoff=args.cutoff, dr=dr).to(device)
+                        config = layers, heads, d_model, dr
 
-                    best_config, val_loss = train(args, model, train_en.to(device), train_de.to(device),
-                          train_y.to(device), valid_en.to(device), valid_de.to(device), valid_y.to(device)
-                          , args.lr, val_loss, config, best_config, path, criterion)
+                        best_config, val_loss = train(args, model, train_en.to(device), train_de.to(device),
+                              train_y.to(device), valid_en.to(device), valid_de.to(device), valid_y.to(device)
+                              , args.lr, val_loss, config, best_config, path, criterion)
 
-    layers, heads, d_model, dr = best_config
-    print(best_config)
+        layers, heads, d_model, dr = best_config
+        print(best_config)
 
+    else:
+        layers, heads, d_model, dr = args.n_layers_best, args.n_heads_best, \
+                                     args.d_model_best, args.dr_best
     d_k = int(d_model / heads)
 
     model = Attn(src_input_size=train_en.shape[3],
@@ -188,7 +197,7 @@ def main():
     test_loss = 0
     for j in range(test_en.shape[0]):
         output = model(test_en[j].to(device), test_de[j].to(device), training=True)
-        y_true = test_y[j]
+        y_true = test_y[j].to(device)
         loss = criterion(y_true, output)
         test_loss += loss.item()
 
