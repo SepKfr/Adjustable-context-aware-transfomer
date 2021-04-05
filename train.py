242234b6b83e6a37f56fcf8ddad7ced38d631eb2
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-05 16:32:29 -0400

    fix error

--- train.py
+++ train.py
@@ -112,7 +112,7 @@ def main():
     parser = argparse.ArgumentParser(description="preprocess argument parser")
     parser.add_argument("--seq_len_pred", type=int, default=64)
     parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--cutoff", type=int, default=[15, 12, 9])
+    parser.add_argument("--cutoff", type=int, default=[15])
     parser.add_argument("--d_model", type=int, default=[64, 32])
     parser.add_argument("--d_model_best", type=int)
     parser.add_argument("--dff", type=int, default=64)
