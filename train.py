0dc8f73a965424e9eaa25031b9d538f3170a50bb
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-09 11:58:38 -0400

    saving the predictions

--- train.py
+++ train.py
@@ -114,9 +114,9 @@ def evaluate(config, args, test_en, test_de, test_y, criterion, seq_len, path):
     n_layers, n_heads, d_model, cutoff, kernel = config
     d_k = int(d_model / n_heads)
     mae = nn.L1Loss()
-    path_to_pred = None
-    if not os.path.exists:
-        path_to_pred = "preds_{}_{}".format(args.site, args.seq_len_pred)
+    path = "preds_{}_{}".format(args.site, args.seq_len_pred)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
     model = Attn(src_input_size=test_en.shape[3],
                  tgt_input_size=test_y.shape[3],
