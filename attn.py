2e8c375e3fe7a69f4976b05c20fcc2fe2d53e67f
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-12 19:38:46 -0400

    add lstm to attn

--- attn.py
+++ attn.py
@@ -111,7 +111,7 @@ class ScaledDotProductAttention(nn.Module):
         if self.attn_type == "con":
             Q = get_con_vecs(Q, self.cutoff).to(self.device)
             K = get_con_vecs(K, self.cutoff).to(self.device)
-            batch_size, n_h, seq_len, cutoff, d_k = Q.shape()
+            batch_size, n_h, seq_len, cutoff, d_k = Q.shape
             Q = Q.reshape(batch_size, n_h, seq_len, cutoff*d_k)
             K = K.reshape(batch_size, n_h, K.shape[2], cutoff*d_k)
             scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / (np.sqrt(self.d_k*cutoff))
