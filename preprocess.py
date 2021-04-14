c5a481f33c5afcf8c6efc41558b23ccf51c46132
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-14 09:06:30 -0400

    add to error multiple times

--- preprocess.py
+++ preprocess.py
@@ -30,7 +30,7 @@ class Data:
         self.derivative = [4, 8, 16, 32]
         self.n_derivative = 0
         self.wavelets = ['db3', 'db5']
-        self.n_wavelets = len(self.wavelets)
+        self.n_wavelets = 0
         self.nf = n_features * (self.n_moving_average + self.n_wavelets + self.n_derivative)
         self.in_seq_len = in_seq_len
         self.out_seq_len = out_seq_len
@@ -130,8 +130,8 @@ class Data:
             data_2d_in[:, i] = self.moving_average(mv, data, ts).squeeze(1)
             #data_2d_in[:, i+self.n_moving_average] = self.get_derivative(mv, data, ts).squeeze(1)
 
-        for i, type in enumerate(self.wavelets):
-            data_2d_in[:, i+self.n_moving_average] = self.create_wavelet(type, data)
+        '''for i, type in enumerate(self.wavelets):
+            data_2d_in[:, i+self.n_moving_average] = self.create_wavelet(type, data)'''
 
         j = 0
         for i in range(0, self.ts):
