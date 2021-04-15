fd037b54a9cc2fcc947464cf9d529ff8d9ff9212
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-15 11:08:07 -0400

    add lstm

--- train_rnn.py
+++ train_rnn.py
@@ -161,7 +161,7 @@ def main():
     parser = argparse.ArgumentParser(description="preprocess argument parser")
     parser.add_argument("--seq_len_pred", type=int, default=64)
     parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--hidden_size", type=int, default=[64])
+    parser.add_argument("--hidden_size", type=int, default=[32, 64])
     parser.add_argument("--kernel", type=int, default=[1, 3, 9])
     parser.add_argument("--dr", type=float, default=0.5)
     parser.add_argument("--lr", type=float, default=0.0001)
