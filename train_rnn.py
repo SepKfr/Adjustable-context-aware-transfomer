a42ff23dd2f47130695178b2bb18eb27a93511c7
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-17 10:31:25 -0400

    normalize for gru and lstm

--- train_rnn.py
+++ train_rnn.py
@@ -145,8 +145,8 @@ def evaluate(config, args, test_en, test_de, test_y, criterion, seq_len, path):
     mae_loss = 0
     for j in range(test_en.shape[0]):
         output = model(test_en[j].to(device), test_de[j].to(device))
-        #output = inverse_transform(output, 'valid').to(device)
-        y_true = test_y[j].to(device)
+        output = inverse_transform(output, 'valid').to(device)
+        y_true = inverse_transform(test_y[j].to(device)).to(device)
         pickle.dump(output, open(os.path.join(path_to_pred, args.name), "wb"))
         loss = criterion(y_true, output)
         test_loss += loss.item()
