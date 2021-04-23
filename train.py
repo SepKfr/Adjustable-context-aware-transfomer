a368f4f3a16c3aa53b6003df0192c3ac01428cd4
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-23 06:25:21 -0400

    revert

--- train.py
+++ train.py
@@ -152,7 +152,7 @@ def main():
     parser = argparse.ArgumentParser(description="preprocess argument parser")
     parser.add_argument("--seq_len_pred", type=int, default=64)
     parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--cutoff", type=int, default=[0, 1, 4])
+    parser.add_argument("--cutoff", type=int, default=[0, 3, 6])
     parser.add_argument("--local", type=int, default=[0])
     parser.add_argument("--cutoff_best", type=int)
     parser.add_argument("--d_model", type=int, default=[32])
