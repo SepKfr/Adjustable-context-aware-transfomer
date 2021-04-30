c40108e0100426a11e2530602bb3fcd6f8f48e2b
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-30 17:47:35 -0400

    remove linear

--- baselines.py
+++ baselines.py
@@ -90,7 +90,7 @@ class RNN(nn.Module):
             en_out, hidden = self.encoder_gru(x_en, hidden)
             outputs, _ = self.decoder_gru(x_de, hidden)
 
-        outputs = self.proj_out(outputs.permute(1, 2, 0))
+        outputs = outputs.permute(1, 2, 0)
         outputs = self.linear2(outputs.permute(0, 2, 1))
 
         return outputs
