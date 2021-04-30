2297e2861f529737971398b5f2934c985c23ffe0
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-04-30 10:22:55 -0400

    revert

--- train.py
+++ train.py
@@ -155,7 +155,7 @@ def main():
     parser.add_argument("--cutoff", type=int, default=[1, 3, 9])
     parser.add_argument("--local", type=int, default=[0])
     parser.add_argument("--cutoff_best", type=int)
-    parser.add_argument("--d_model", type=int, default=[64])
+    parser.add_argument("--d_model", type=int, default=[32])
     parser.add_argument("--d_model_best", type=int)
     parser.add_argument("--dff", type=int, default=64)
     parser.add_argument("--n_heads", type=list, default=[1, 8])
