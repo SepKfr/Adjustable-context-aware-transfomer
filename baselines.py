90b9bbe950eb4edd866b393ec2516240c3649292
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-05-05 07:02:47 -0400

    add to baselines

--- baselines.py
+++ baselines.py
@@ -67,9 +67,9 @@ class RNConv(nn.Module):
         super(RNConv, self).__init__()
         self.n_layers = n_layers
         self.hidden_size = hidden_size
-        self.conv = [nn.Conv1d(input_size, out_channel, kernel) for _ in range(n_layers)]
+        self.conv = nn.Conv1d(input_size, out_channel, kernel)
         self.lstm = nn.LSTM(out_channel, hidden_size, n_layers, dropout=d_r)
-        self.dropout1 = [nn.Dropout(d_r) for _ in range(n_layers)]
+        self.dropout1 = nn.Dropout(d_r)
         self.linear2 = nn.Linear(out_channel, output_size)
         self.proj_out = nn.Linear(seq_len, seq_pred_len, bias=False)
         self.kernel_size = kernel
@@ -86,8 +86,8 @@ class RNConv(nn.Module):
         x = F.pad(x, (padding, 0))
 
         for i in range(self.n_layers):
-            x_out = self.conv[i](x)
-            x_out = self.dropout1[i](x_out)
+            x_out = self.conv(x)
+            x_out = self.dropout1(x_out)
 
         x_en_out = x_out.view(seq_len, b, -1)
 
