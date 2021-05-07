96af5d17383934b8d8cbf756e2235c756041519c
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-05-07 11:52:33 -0400

    adding wavelet to features

--- attn.py
+++ attn.py
@@ -110,9 +110,10 @@ class ScaledDotProductAttention(nn.Module):
         if self.attn_type == "con":
             Q = get_con_vecs(Q, self.cutoff).to(self.device)
             K = get_con_vecs(K, self.cutoff).to(self.device)
-            V = K
             batch_size, n_h, seq_len, cutoff, d_k = Q.shape
-            scores = torch.einsum('bhqcd,bhkcd->bhqk', Q, K) / (np.sqrt(self.d_k*cutoff))
+            scores = torch.einsum('bhqcd,bhkcd->bhqkc', Q, K) / (np.sqrt(self.d_k*cutoff))
+            if attn_mask is not None:
+                attn_mask = attn_mask.unsqueeze(4).repeat(1, 1, 1, 1, cutoff)
         else:
             scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
 
@@ -124,7 +125,12 @@ class ScaledDotProductAttention(nn.Module):
 
         attn = nn.Softmax(dim=-1)(scores)
 
-        context = torch.einsum('bhqk,bhvd->bhqd', attn, V)
+        if self.attn_type == "con":
+            context = torch.einsum('bhqkc,bhvd->bhqd', attn, V)
+            attn = torch.einsum('bhqkc->bhqk', attn)
+
+        else:
+            context = torch.einsum('bhqk,bhvd->bhqd', attn, V)
         return context, attn
 
 
