9000ee0d3b14b82ccecfd9ab818a3af1fcadb99a
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-05-07 11:31:30 -0400

    adding wavelet to features

--- attn.py
+++ attn.py
@@ -112,7 +112,7 @@ class ScaledDotProductAttention(nn.Module):
             K = get_con_vecs(K, self.cutoff).to(self.device)
             V = K
             batch_size, n_h, seq_len, cutoff, d_k = Q.shape
-            scores = torch.einsum('bhqcd,bhkcd->bhqkc', Q, K) / (np.sqrt(self.d_k*cutoff))
+            scores = torch.einsum('bhqcd,bhkcd->bhqk', Q, K) / (np.sqrt(self.d_k*cutoff))
         else:
             scores = torch.einsum('bhqd,bhkd->bhqk', Q, K) / np.sqrt(self.d_k)
 
@@ -125,8 +125,7 @@ class ScaledDotProductAttention(nn.Module):
         attn = nn.Softmax(dim=-1)(scores)
 
         if self.attn_type == "con":
-            context = torch.einsum('bhqkc,bhvcd->bhqd', attn, V)
-            attn = torch.einsum('bhqkc->bhqk', attn)
+            context = torch.einsum('bhqk,bhvcd->bhqd', attn, V)
 
         else:
             context = torch.einsum('bhqk,bhvd->bhqd', attn, V)
