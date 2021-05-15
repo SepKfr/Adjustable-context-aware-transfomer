91819acb7dd01ccfbe27b34e6c4822d2078ba8ea
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-05-15 19:43:46 -0400

    evaluation

--- eval.py
+++ eval.py
@@ -74,10 +74,10 @@ def evaluate(site, seq_ln):
     plt.vlines(143, ymin=min(torch.min(x_true_c[best_ind, :] - 10), torch.min(y_true_c[best_ind, :] - 10)),
                ymax=max(torch.max(x_true_c[best_ind, :]), torch.max(y_true_c[best_ind, :])), colors='lightblue', linestyles ="dashed")
     plt.plot(np.arange(143, 216), y_true_c[best_ind, :], color='chocolate')
-    plt.plot(np.arange(143, 216), preds_attn_con[best_ind, :].detach().numpy(), color='deepskyblue')
-    plt.plot(np.arange(143, 216), preds_attn[best_ind, :].detach().numpy(), color='seagreen')
-    plt.plot(np.arange(143, 216), preds_attn_conv[best_ind, :].detach().numpy(), color='orange')
-    plt.plot(np.arange(143, 216), preds_lstm[best_ind, :].detach().numpy(), color='salmon')
+    plt.plot(np.arange(143, 216), preds_attn_con[best_ind, :].cpu().detach().numpy(), color='deepskyblue')
+    plt.plot(np.arange(143, 216), preds_attn[best_ind, :].cpu().detach().numpy(), color='seagreen')
+    plt.plot(np.arange(143, 216), preds_attn_conv[best_ind, :].cpu().detach().numpy(), color='orange')
+    plt.plot(np.arange(143, 216), preds_lstm[best_ind, :].cpu().detach().numpy(), color='salmon')
     plt.savefig('pred_plot_{}.png'.format(site))
     plt.close()
 
