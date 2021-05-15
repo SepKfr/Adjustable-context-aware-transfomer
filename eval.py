f1350f185eb413a8406c86d8a66c000cea0a1404
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-05-15 15:28:55 -0400

    evaluation

--- eval.py
+++ eval.py
@@ -1,9 +1,10 @@
 import pickle
-from preprocess import Scaler
-from utils import Metrics
+import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
 import numpy as np
 import argparse
+import torch
 
 train_x = pickle.load(open("train_x.p", "rb"))
 train_y = pickle.load(open("train_y.p", "rb"))
@@ -11,7 +12,8 @@ valid_x = pickle.load(open("valid_x.p", "rb"))
 valid_y = pickle.load(open("valid_y.p", "rb"))
 test_x = pickle.load(open("test_x.p", "rb"))
 test_y = pickle.load(open("test_y.p", "rb"))
-
+x_true = test_x[:, :, 6]
+y_true = torch.cat((x_true[:, -1].unsqueeze(-1),test_y[:, :, 0]),dim=-1)
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
@@ -21,10 +23,51 @@ else:
     print("running on CPU")
 
 
-def evaluate(site, seq_ln, name):
+rmses = dict()
 
-    preds = pickle.load(open('{}_{}_{}/{}'.format('Preds/preds', site, seq_ln, name),'rb'))
-    print(preds)
+def plot_predictions(num):
+
+    plt.plot(np.arange(0, 144), x_true[num, :])
+    plt.vlines(143, ymin=min(torch.min(x_true[num, :]), torch.min(y_true[num, :])),
+               ymax=max(torch.max(x_true[num, :]), torch.max(y_true[num, :])), colors='purple', ls='--')
+    plt.plot(np.arange(143, 216), y_true[num, :])
+    plt.show()
+
+
+def get_rmse(pred, num, criterion):
+    rmse = torch.zeros(pred.shape(0))
+    for i in range(pred.shape(0)):
+        rmses[i] = torch.sqrt(criterion(y_true[num, i], pred[num, i]))
+    return rmse
+
+
+def evaluate(site, seq_ln):
+
+    criterion = nn.MSELoss()
+    preds_attn_con = pickle.load(open('{}_{}_{}/{}'.format('Preds/preds', site, seq_ln, 'attn_con_2'), 'rb'))
+    preds_attn = pickle.load(open('{}_{}_{}/{}'.format('Preds/preds', site, seq_ln, 'attn'), 'rb'))
+    preds_attn_conv = pickle.load(open('{}_{}_{}/{}'.format('Preds/preds', site, seq_ln, 'attn_conv'), 'rb'))
+    preds_lstm = pickle.load(open('{}_{}_{}/{}'.format('Preds/preds', site, seq_ln, 'lstm'), 'rb'))
+
+    best_rmse = 1e5
+    best_ind = 0
+    for i in range(len(test_y)):
+        rmse = torch.sqrt(criterion(preds_attn_con[i, :, :], test_y[i]))
+        if rmse < best_rmse:
+            best_rmse = rmse
+            best_ind = i
+
+    rmses["ours"] = get_rmse(preds_attn_con, best_ind, criterion)
+    rmses["attn"] = get_rmse(preds_attn_con, best_ind, criterion)
+    rmses["attn_conv"] = get_rmse(preds_attn_con, best_ind, criterion)
+    rmses["lstm"] = get_rmse(preds_attn_con, best_ind, criterion)
+
+    x = np.linspace(0,8,72)
+    plt.plot(x, rmses.get("ours")[0::8], 'xb-')
+    plt.savefig('rmses_{}.png'.format(site))
+
+
+
 
 
 def main():
@@ -34,7 +77,7 @@ def main():
     parser.add_argument("--site", type=str, default="WHB")
     parser.add_argument("--name", type=str, default="attn")
     params = parser.parse_args()
-    evaluate(params.site, params.seq_len, params.name)
+    evaluate(params.site, params.seq_len)
 
 
 if __name__ == '__main__':
