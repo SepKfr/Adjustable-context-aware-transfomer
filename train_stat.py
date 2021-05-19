cb6fc29a9a8ac4149a63faec6540cd22112019f5
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-05-19 14:45:07 -0400

    fixed an error

--- train_stat.py
+++ train_stat.py
@@ -6,10 +6,11 @@ def main():
     test_x = pickle.load(open("test_x.p", "rb"))
     test_y = pickle.load(open("test_y.p", "rb"))
 
+    print(test_x[0, :, :].detach().numpy())
     for seq in range(len(test_x)):
-        model = AutoReg(test_x[0, :, :].detach().numpy(), lags=len(test_y[0, :, :]))
+        model = AutoReg(test_x[seq, :, :].detach().numpy(), lags=len(test_y[seq, :, :]))
         model_fit = model.fit()
-        preds = model_fit.predict(start=len(test_x[0, :, :]), end=(len(test_x[0, :, :] + len(test_y[0, :, :]) - 1)))
+        preds = model_fit.predict(start=len(test_x[0, :, :]), end=(len(test_x[seq, :, :] + len(test_y[0, :, :]) - 1)))
 
 
 if __name__ == '__main__':
