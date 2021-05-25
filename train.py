f0b275e6e33c24c9be31eb19421e025d14f10ae0
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-05-25 14:16:00 -0400

    trying cosine similarity

--- train.py
+++ train.py
@@ -193,7 +193,7 @@ def evaluate(config, args, test_en, test_de, test_y, criterion, seq_len, path):
 def main():
 
     parser = argparse.ArgumentParser(description="preprocess argument parser")
-    parser.add_argument("--seq_len_pred", type=int, default=1)
+    parser.add_argument("--seq_len_pred", type=int, default=72)
     parser.add_argument("--batch_size", type=int, default=48)
     parser.add_argument("--cutoff", type=int, default=[1, 3, 6, 9])
     parser.add_argument("--cutoff_best", type=int)
@@ -211,7 +211,7 @@ def main():
     parser.add_argument("--n_epochs", type=int, default=1)
     parser.add_argument("--run_num", type=int, default=1)
     parser.add_argument("--pos_enc", type=str, default='sincos')
-    parser.add_argument("--attn_type", type=str, default='attn')
+    parser.add_argument("--attn_type", type=str, default='con')
     parser.add_argument("--name", type=str, default='attn')
     parser.add_argument("--site", type=str, default="WHB")
     parser.add_argument("--server", type=str, default="c01")
