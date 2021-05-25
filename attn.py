4edbe222c0b21507c1b9446af093e0c26800fe9e
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-05-25 16:47:46 -0400

    attn version from paper attention is all you need

--- attn.py
+++ attn.py
@@ -71,20 +71,6 @@ class GELU(nn.Module):
         return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
 
 
-class LayerNorm(nn.Module):
-    "Construct a layernorm module (See citation for details)."
-    def __init__(self, features, eps=1e-6):
-        super(LayerNorm, self).__init__()
-        self.a_2 = nn.Parameter(torch.ones(features))
-        self.b_2 = nn.Parameter(torch.zeros(features))
-        self.eps = eps
-
-    def forward(self, x):
-        mean = x.mean(-1, keepdim=True)
-        std = x.std(-1, keepdim=True)
-        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
-
-
 class PositionalEncoding(nn.Module):
 
     def __init__(self, d_model, dropout, device, max_seq_len=500):
