53f5a70492c2b709dca82bb949e786d9995f42bf
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-06-04 17:11:32 -0400

    try traffic and electricity data set

--- attn_train.py
+++ attn_train.py
@@ -113,9 +113,6 @@ def train(args, model, train_en, train_de, train_y, train_id,
 
             e = epoch
 
-        if epoch - e > 20:
-            stop = True
-
         if epoch % 20 == 0:
             print("Average loss: {:.3f}".format(test_loss))
 
