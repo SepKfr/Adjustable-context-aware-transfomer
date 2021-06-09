4c0dc11e16c01efbe6ea99958ac0efac87c5bc03
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-06-09 10:24:16 -0400

    adding quantile loss

--- air_quality.py
+++ air_quality.py
@@ -158,5 +158,5 @@ class AirQualityFormatter(GenericDataFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 48000, 960
+        return 16000, 960
 
