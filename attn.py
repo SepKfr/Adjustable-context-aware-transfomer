2e7a45c5716dd7dc0f45834b5635fe198953d06a
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-06-11 11:44:33 -0400

    adding numba

--- attn.py
+++ attn.py
@@ -112,30 +112,20 @@ class ScaledDotProductAttention(nn.Module):
         b, h, l, d_k = Q.shape
         l_k = K.shape[2]
 
-        if self.attn_type == "temp" or self.attn_type == "temp_2":
+        if self.attn_type == "temp":
 
-            n_k = math.floor(math.log2(l)) + 1
-
-            if self.attn_type == "temp_2":
-                V_p = torch.zeros(b, h, n_k, l_k, d_k)
-            scores = torch.zeros(b, h, n_k, l, l_k)
+            n_k = [1, 3, 6, 9]
+            scores = torch.zeros(b, h, len(n_k), l, l_k)
 
             ind = 0
-            for k in range(0, n_k):
-                k = 2*k + 1
+            for k in n_k:
                 Q_g = get_con_vecs(Q, k)
                 K_g = get_con_vecs(K, k)
                 scores[:, :, ind, :, :] = torch.einsum('bhqcd,bhkcd->bhqk', Q_g, K_g) / np.sqrt(self.d_k)
-                if self.attn_type == "temp_2":
-                    V_g = nn.Linear(k, 1).to(self.device)(K_g.transpose(-1, -2)).transpose(-1, -2).squeeze(3)
-                    V_p[:, :, ind, :, :] = V_g
                 ind += 1
-
-            if self.attn_type == "temp_2":
-                V = V_p.to(self.device)
             scores = scores.to(self.device)
             if attn_mask is not None:
-                attn_mask = attn_mask.unsqueeze(2).repeat(1, 1, n_k, 1, 1)
+                attn_mask = attn_mask.unsqueeze(2).repeat(1, 1, len(n_k), 1, 1)
 
         elif self.attn_type == "conv_attn":
 
@@ -163,13 +153,10 @@ class ScaledDotProductAttention(nn.Module):
 
         attn = nn.Softmax(dim=-1)(scores)
 
-        if self.attn_type == "temp" or self.attn_type == "temp_2":
+        if self.attn_type == "temp":
 
             attn = nn.Softmax(dim=-3)(scores)
-            if self.attn_type == "temp_2":
-                context = torch.einsum('bhgqk,bhgkd->bhqd', attn, V)
-            else:
-                context = torch.einsum('bhgqk,bhkd->bhqd', attn, V)
+            context = torch.einsum('bhgqk,bhkd->bhqd', attn, V)
 
             attn = torch.einsum('bhgqk->bhqk', attn)
         else:
