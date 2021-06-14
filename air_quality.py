b5bb19357f755c41314e8a686b87f2aec9d6b353
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-06-14 18:52:44 -0400

    normalize loss

--- air_quality.py
+++ air_quality.py
@@ -68,5 +68,5 @@ class AirQualityFormatter(ElectricityFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 450000, 50000
+        return 256000, 12800
 
