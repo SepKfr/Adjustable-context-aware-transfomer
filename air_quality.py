fca762ad409cff83827e45d3369b93a5b9c83516
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-06-20 16:54:05 -0400

    increase the data

--- air_quality.py
+++ air_quality.py
@@ -60,7 +60,7 @@ class AirQualityFormatter(ElectricityFormatter):
 
         model_params = {
             'dropout_rate': 0.3,
-            'hidden_layer_size': 128,
+            'hidden_layer_size': 32,
             'learning_rate': 0.001,
             'minibatch_size': 128,
             'max_gradient_norm': 100.,
