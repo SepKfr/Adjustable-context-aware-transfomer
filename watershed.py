5c2f3773f8579686087c3dcc0af7f68b03a4a81a
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-06-21 18:28:35 -0400

    adding layer norms

--- watershed.py
+++ watershed.py
@@ -91,4 +91,4 @@ class WatershedFormatter(DataFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 1280, 512
+        return 128000, 5120
