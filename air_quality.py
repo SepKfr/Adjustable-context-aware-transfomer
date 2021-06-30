80d24482493f3cfd01008e5bd3574203cfedcd8b
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-06-30 12:03:56 -0400

    adding argmax to the model

--- air_quality.py
+++ air_quality.py
@@ -77,4 +77,4 @@ class AirQualityFormatter(ElectricityFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 128000, 16000
+        return 64000, 8000
