aced0975519767d2b046312ef244cb2d9f19fc82
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-06-30 13:42:31 -0400

    less data

--- air_quality.py
+++ air_quality.py
@@ -77,4 +77,4 @@ class AirQualityFormatter(ElectricityFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 64000, 8000
+        return 32000, 4000
