843bc5b38541e4416a20ab10605d02d382a73729
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-01 13:15:37 -0400

    16 feature size

--- electricity.py
+++ electricity.py
@@ -197,7 +197,7 @@ class ElectricityFormatter(GenericDataFormatter):
 
         model_params = {
             'dropout_rate': 0.3,
-            'hidden_layer_size': [16, 64],
+            'hidden_layer_size': [16],
             'learning_rate': 0.001,
             'minibatch_size': 64,
             'max_gradient_norm': 100.,
--- watershed.py
+++ watershed.py
@@ -74,7 +74,7 @@ class WatershedFormatter(DataFormatter):
 
         model_params = {
             'dropout_rate': 0.3,
-            'hidden_layer_size': [16, 64],
+            'hidden_layer_size': [16],
             'learning_rate': 0.001,
             'minibatch_size': 64,
             'max_gradient_norm': 100.,
