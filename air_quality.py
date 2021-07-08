60ace20c6c57f278e6cb1890ef5ab1917cd9fd13
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-08 08:29:20 -0400

    new version of atten temp

--- air_quality.py
+++ air_quality.py
@@ -60,7 +60,7 @@ class AirQualityFormatter(ElectricityFormatter):
 
         model_params = {
             'dropout_rate': 0.3,
-            'hidden_layer_size': [32],
+            'hidden_layer_size': [64],
             'learning_rate': 0.001,
             'minibatch_size': 64,
             'max_gradient_norm': 100.,
