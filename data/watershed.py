6b3d3579bded275a82db810859c6126272310d2b
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-16 19:50:52 -0400

    change part of air quality for train and test split

--- data/watershed.py
+++ data/watershed.py
@@ -34,7 +34,7 @@ class WatershedFormatter(DataFormatter):
         ('categorical_id', DataTypes.CATEGORICAL, InputTypes.STATIC_INPUT),
     ]
 
-    def split_data(self, df, valid_boundary=1007, test_boundary=1557):
+    def split_data(self, df, valid_boundary=907, test_boundary=1507):
         """Splits data frame into training-validation-test data frames.
         This also calibrates scaling object, and transforms data for each split.
         Args:
