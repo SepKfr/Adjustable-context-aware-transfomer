ef2e4da3b80c8aa5dd0b1370b4d912d9e622f766
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-15 20:35:21 -0400

    change test valid split of solar

--- data/watershed.py
+++ data/watershed.py
@@ -70,7 +70,7 @@ class WatershedFormatter(DataFormatter):
 
         model_params = {
             'dropout_rate': 0.3,
-            'hidden_layer_size': [32],
+            'hidden_layer_size': [16],
             'learning_rate': 0.001,
             'minibatch_size': 64,
             'max_gradient_norm': 100.,
