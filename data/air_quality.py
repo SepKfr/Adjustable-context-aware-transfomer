c2662f934be8eed971b58e0c7950cfa9209bdd36
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-17 09:33:12 -0400

    change part of air quality for train and test split

--- data/air_quality.py
+++ data/air_quality.py
@@ -30,7 +30,7 @@ class AirQualityFormatter(ElectricityFormatter):
         ('id', DataTypes.REAL_VALUED, InputTypes.ID),
     ]
 
-    def split_data(self, df, valid_boundary=1160, test_boundary=1310):
+    def split_data(self, df, valid_boundary=1060, test_boundary=1460):
         print('Formatting train-valid-test splits.')
 
         index = df['days_from_start']
