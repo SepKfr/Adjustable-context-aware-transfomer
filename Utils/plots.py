32b3753d67bb486b12c366eed60126b34ba9aae1
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-27 13:56:01 -0400

    adding plot for predictions

--- Utils/plots.py
+++ Utils/plots.py
@@ -15,9 +15,9 @@ def main():
     parser.add_argument("--seed", type=int, default=21)
     args = parser.parse_args()
 
-    y_true = pickle.load(open('y_true_{}.pkl'.format(args.exp_name), 'rb')).cpu().detach()
+    y_true = pickle.load(open('y_true_{}.pkl'.format(args.exp_name), 'rb'))
     print("read y_true")
-    y_true_input = pickle.load(open('y_true_input_{}.pkl'.format(args.exp_name), 'rb')).cpu().detach()
+    y_true_input = pickle.load(open('y_true_input_{}.pkl'.format(args.exp_name), 'rb'))
     print("read y_true_input")
 
     rand_ind = random.randint(0, 8000)
@@ -27,31 +27,27 @@ def main():
     plt.plot(np.arange(0, 192), np.concatenate((y_true_input.iloc[rand_ind, :-1], y_true.iloc[rand_ind, :-1])),
              color='blue')
 
-
     lstm = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
-                                         'lstm_{}'.format(args.seed)), 'rb')).cpu().detach()
+                                         'lstm_{}'.format(args.seed)), 'rb'))
     print("read lstm")
     plt.plot(np.arange(168, 192), lstm.iloc[rand_ind, :-1], color='navy')
 
-
     attn = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
-                                         'attn_{}'.format(args.seed)), 'rb')).cpu().detach()
+                                         'attn_{}'.format(args.seed)), 'rb'))
     print("read attn")
     plt.plot(np.arange(168, 192), attn.iloc[rand_ind, :-1], color='violet')
 
-
     attn_conv = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_conv_{}'.format(args.seed))
-                                 , 'rb')).cpu().detach()
+                                 , 'rb'))
     print("read conv attn")
     plt.plot(np.arange(168, 192), attn_conv.iloc[rand_ind, :-1], color='seagreen')
 
     attn_temp_cutoff = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_temp_cutoff_{}'
-                                                     .format(args.seed)), 'rb')).cpu().detach()
+                                                     .format(args.seed)), 'rb'))
     print("read attn temp cutoff")
     plt.plot(np.arange(168, 192), attn_temp_cutoff.iloc[rand_ind, :-1], color='orange')
 
     print("plotting...")
-
     plt.vlines(168, y_min=0, ymax=max(y_true.iloc[rand_ind, :-1]), colors='lightblue', linestyles="dashed")
 
     plt.title(args.exp_name)
