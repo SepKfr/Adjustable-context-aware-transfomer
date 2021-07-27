3ddd7c0adfcd181462b96ba325dbe9b6f0b20bed
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-27 18:16:19 -0400

    adding plot for predictions

--- Utils/plots.py
+++ Utils/plots.py
@@ -4,36 +4,10 @@ import os
 import numpy as np
 import random
 import argparse
+import torch.nn as nn
 
 
-def main():
-
-    parser = argparse.ArgumentParser("plots for predictions")
-    parser.add_argument("--exp_name", type=str, default="favorita")
-    parser.add_argument("--cuda", type=str, default='cuda:0')
-    parser.add_argument("--seed", type=int, default=21)
-    args = parser.parse_args()
-
-    y_true = pickle.load(open('y_true_{}.pkl'.format(args.exp_name), 'rb'))
-    print("read y_true")
-    y_true_input = pickle.load(open('y_true_input_{}.pkl'.format(args.exp_name), 'rb'))
-    print("read y_true_input")
-
-    lstm = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
-                                         'lstm_{}'.format(args.seed)), 'rb'))
-    print("read lstm")
-
-    attn = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
-                                         'attn_{}'.format(args.seed)), 'rb'))
-    print("read attn")
-    attn_conv = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_conv_{}'.format(args.seed))
-                                 , 'rb'))
-    print("read conv attn")
-
-    attn_temp_cutoff = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_temp_cutoff_{}'
-                                                     .format(args.seed)), 'rb'))
-    print("read attn temp cutoff")
-
+def plot(args, y_true, y_true_input, lstm, attn, attn_conv, attn_temp_cutoff):
     print("plotting...")
     rand_ind = random.randint(0, 8000)
     print(lstm.iloc[rand_ind, :-1])
@@ -55,6 +29,37 @@ def main():
     plt.legend(['ground-truth', 'seq2seq-lstm', 'attn', 'conv attn', 'ours'], loc="upper left")
     plt.savefig('pred_plot_{}.png'.format(args.exp_name))
     plt.close()
+
+def main():
+
+    parser = argparse.ArgumentParser("plots for predictions")
+    parser.add_argument("--exp_name", type=str, default="favorita")
+    parser.add_argument("--cuda", type=str, default='cuda:0')
+    parser.add_argument("--seed", type=int, default=21)
+    args = parser.parse_args()
+
+    predictions_lstm = np.ndarray((3, 8000, 24))
+    predictions_attn = np.ndarray((3, 8000, 24))
+    predictions_attn_conv = np.ndarray((3, 8000, 24))
+    predictions_attn_temp = np.ndarray((3, 8000, 24))
+
+    y_true = pickle.load(open('y_true_{}.pkl'.format(args.exp_name), 'rb')).values.cpu().detach()
+    print("read y_true")
+    '''y_true_input = pickle.load(open('y_true_input_{}.pkl'.format(args.exp_name), 'rb'))
+    print("read y_true_input")'''
+
+    seeds = [21, 9, 1992]
+    for i, seed in enumerate(seeds):
+        predictions_lstm[i, :, :] = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
+                                             'lstm_{}'.format(seed)), 'rb')).values.cpu().detach()
+        predictions_attn[i, :, :] = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
+                                             'attn_{}'.format(seed)), 'rb')).values.cpu().detach()
+        predictions_attn_conv[i, :, :] = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
+                                                  'attn_conv_{}'.format(seed)), 'rb')).values.cpu().detach()
+        predictions_attn_temp[i, :, :] = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_temp_cutoff_{}'
+                                                         .format(seed)), 'rb')).values.cpu().detach()
+
+    RMSE = nn.MSELoss()
 
 
 if __name__ == '__main__':
