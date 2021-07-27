c8d9299672e8e2aa40929ad0adadc00e58a747ae
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-27 13:53:36 -0400

    adding plot for predictions

--- Utils/plots.py
+++ Utils/plots.py
@@ -15,9 +15,9 @@ def main():
     parser.add_argument("--seed", type=int, default=21)
     args = parser.parse_args()
 
-    y_true = pickle.load(open('y_true_{}.pkl'.format(args.exp_name), 'rb'))
+    y_true = pickle.load(open('y_true_{}.pkl'.format(args.exp_name), 'rb')).cpu().detach()
     print("read y_true")
-    y_true_input = pickle.load(open('y_true_input_{}.pkl'.format(args.exp_name), 'rb'))
+    y_true_input = pickle.load(open('y_true_input_{}.pkl'.format(args.exp_name), 'rb')).cpu().detach()
     print("read y_true_input")
 
     rand_ind = random.randint(0, 8000)
@@ -26,33 +26,33 @@ def main():
     plt.rc('legend', fontsize=12)
     plt.plot(np.arange(0, 192), np.concatenate((y_true_input.iloc[rand_ind, :-1], y_true.iloc[rand_ind, :-1])),
              color='blue')
-    del y_true
-    del y_true_input
 
-    lstm = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'lstm_{}'.format(args.seed)), 'rb'))
+
+    lstm = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
+                                         'lstm_{}'.format(args.seed)), 'rb')).cpu().detach()
     print("read lstm")
     plt.plot(np.arange(168, 192), lstm.iloc[rand_ind, :-1], color='navy')
-    del lstm
 
-    attn = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_{}'.format(args.seed)), 'rb'))
+
+    attn = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
+                                         'attn_{}'.format(args.seed)), 'rb')).cpu().detach()
     print("read attn")
     plt.plot(np.arange(168, 192), attn.iloc[rand_ind, :-1], color='violet')
-    del attn
+
 
     attn_conv = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_conv_{}'.format(args.seed))
-                                 , 'rb'))
+                                 , 'rb')).cpu().detach()
     print("read conv attn")
     plt.plot(np.arange(168, 192), attn_conv.iloc[rand_ind, :-1], color='seagreen')
-    del attn_conv
 
     attn_temp_cutoff = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_temp_cutoff_{}'
-                                                     .format(args.seed)), 'rb'))
+                                                     .format(args.seed)), 'rb')).cpu().detach()
     print("read attn temp cutoff")
     plt.plot(np.arange(168, 192), attn_temp_cutoff.iloc[rand_ind, :-1], color='orange')
-    del attn_temp_cutoff
 
     print("plotting...")
-    plt.vlines(168, colors='lightblue', linestyles="dashed")
+
+    plt.vlines(168, y_min=0, ymax=max(y_true.iloc[rand_ind, :-1]), colors='lightblue', linestyles="dashed")
 
     plt.title(args.exp_name)
     plt.xlabel('TimeSteps')
