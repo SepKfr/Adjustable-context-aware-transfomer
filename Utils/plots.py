af5be1fa234d7866c27f0ca72cf0c4eca6ba134f
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-27 14:21:27 -0400

    adding plot for predictions

--- Utils/plots.py
+++ Utils/plots.py
@@ -4,7 +4,6 @@ import os
 import numpy as np
 import random
 import argparse
-import torch
 
 
 def main():
@@ -20,6 +19,22 @@ def main():
     y_true_input = pickle.load(open('y_true_input_{}.pkl'.format(args.exp_name), 'rb'))
     print("read y_true_input")
 
+    lstm = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
+                                         'lstm_{}'.format(args.seed)), 'rb'))
+    print("read lstm")
+
+    attn = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
+                                         'attn_{}'.format(args.seed)), 'rb'))
+    print("read attn")
+    attn_conv = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_conv_{}'.format(args.seed))
+                                 , 'rb'))
+    print("read conv attn")
+
+    attn_temp_cutoff = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_temp_cutoff_{}'
+                                                     .format(args.seed)), 'rb'))
+    print("read attn temp cutoff")
+
+    print("plotting...")
     rand_ind = random.randint(0, 8000)
     plt.rc('axes', labelsize=18)
     plt.rc('axes', titlesize=18)
@@ -27,33 +42,16 @@ def main():
     plt.plot(np.arange(0, 192), np.concatenate((y_true_input.iloc[rand_ind, :-1], y_true.iloc[rand_ind, :-1])),
              color='blue')
 
-    lstm = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
-                                         'lstm_{}'.format(args.seed)), 'rb'))
-    print("read lstm")
-    plt.plot(np.arange(168, 192), lstm.iloc[rand_ind, :-1], color='navy')
-
-    attn = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
-                                         'attn_{}'.format(args.seed)), 'rb'))
-    print("read attn")
-    plt.plot(np.arange(168, 192), attn.iloc[rand_ind, :-1], color='violet')
-
-    attn_conv = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_conv_{}'.format(args.seed))
-                                 , 'rb'))
-    print("read conv attn")
-    plt.plot(np.arange(168, 192), attn_conv.iloc[rand_ind, :-1], color='seagreen')
-
-    attn_temp_cutoff = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name), 'attn_temp_cutoff_{}'
-                                                     .format(args.seed)), 'rb'))
-    print("read attn temp cutoff")
-    plt.plot(np.arange(168, 192), attn_temp_cutoff.iloc[rand_ind, :-1], color='orange')
-
-    print("plotting...")
     plt.vlines(168, ymin=0, ymax=max(y_true.iloc[rand_ind, :-1]), colors='lightblue', linestyles="dashed")
+    plt.plot(np.arange(168, 192), lstm.iloc[rand_ind, :-1], color='navy')
+    plt.plot(np.arange(168, 192), attn.iloc[rand_ind, :-1], color='violet')
+    plt.plot(np.arange(168, 192), attn_conv.iloc[rand_ind, :-1], color='seagreen')
+    plt.plot(np.arange(168, 192), attn_temp_cutoff.iloc[rand_ind, :-1], color='orange')
 
     plt.title(args.exp_name)
     plt.xlabel('TimeSteps')
     plt.ylabel('Y')
-    plt.legend(['ground-truth', 'seq2seq-lstm', 'attn', 'conv attn', 'ours'], loc="upper right")
+    plt.legend(['ground-truth', 'seq2seq-lstm', 'attn', 'conv attn', 'ours'], loc="upper left")
     plt.savefig('pred_plot_{}.png'.format(args.exp_name))
     plt.close()
 
