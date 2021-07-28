1123a6ed62094ad4b6fa4160bfc078378bd02425
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-28 16:54:33 -0400

    plotting rmses

--- Utils/plots.py
+++ Utils/plots.py
@@ -53,9 +53,9 @@ def main():
 
     seeds = [21, 9, 1992]
     for i, seed in enumerate(seeds):
-        predictions_lstm[i, :, :] = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
+        lstm = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
                                              'lstm_{}'.format(seed)), 'rb'))
-        print(predictions_lstm[i, :, :])
+        print(lstm)
         predictions_attn[i, :, :] = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
                                              'attn_{}'.format(seed)), 'rb')).iloc[:, :-1].to_numpy().astype('float32')
         predictions_attn_conv[i, :, :] = pickle.load(open(os.path.join('preds_{}_24'.format(args.exp_name),
