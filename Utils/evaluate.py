a7cc14591c85ed04b37c917a910cc57f7f498520
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-29 11:14:58 -0400

    adding evaluate.py

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -107,6 +107,7 @@ def read_models(args, device, test_en, test_de, test_y, test_id, formatter):
 def main():
     parser = argparse.ArgumentParser("Analysis of the models")
     parser.add_argument('--exp_name', type=str, default='traffic')
+    parser.add_argument('--cuda', type=str, default='cuda:0')
     args = parser.parse_args()
 
     device = torch.device(args.cuda if torch.cuda.is_available() else "cpu")
