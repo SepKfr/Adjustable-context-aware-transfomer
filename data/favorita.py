da45211140253655a1e0c5f0fc8e53c882b53d4a
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-07-31 19:54:41 -0400

    removing target from decoder

--- data/favorita.py
+++ data/favorita.py
@@ -267,7 +267,7 @@ class FavoritaFormatter(base.GenericDataFormatter):
     Returns:
       Tuple of (training samples, validation samples)
     """
-    return 450000, 50000
+    return 64000, 8000
 
   def get_column_definition(self):
     """"Formats column definition in order expected by the TFT.
--- data/watershed.py
+++ data/watershed.py
@@ -84,4 +84,4 @@ class WatershedFormatter(DataFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 450000, 50000
+        return 64000, 8000
