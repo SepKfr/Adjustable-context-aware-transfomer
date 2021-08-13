030164883bf796dcbfd8ee6330d173f4bea1cfa2
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-08-13 14:26:07 -0400

    saving flow rate and conductivity for predictions for one example

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -108,16 +108,13 @@ def read_models(args, device, test_en, test_de, test_y, test_id, formatter):
             if not flg:
                 targets = extract_numerical_data(
                     formatter.format_predictions(output_map["targets"])).to_numpy().astype('float32')
-                print(test_en.shape)
-                print(test_en[j, :, 4].shape)
+
                 x = extract_numerical_data(
-                    formatter.format_predictions(format_outputs(test_en[j, :, 4].unsqueeze(-1), test_id[j]))
+                    formatter.format_predictions(format_outputs(test_en[j, :, :, 4], test_id[j]))
                 )
-                print(x.shape)
-                print(flow_rate_prefix.shape)
                 flow_rate_prefix[j, :, :] = x
                 flow_rate_postfix[j, :, :] = extract_numerical_data(
-                    formatter.format_predictions(format_outputs(test_de[j, :, 3].unsqueeze(-1), test_id[j]))
+                    formatter.format_predictions(format_outputs(test_de[j, :, :, 3], test_id[j]))
                 )
                 targets_all[j, :, :] = targets
                 targets_all_input[j, :, :] = extract_numerical_data(formatter.format_predictions
