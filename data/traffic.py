a1f147876c10cbd13ac76e4d3c3828d5b90f8c21
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-08-17 11:33:15 -0400

    running multi-layer attention

--- data/traffic.py
+++ data/traffic.py
@@ -139,7 +139,7 @@ class TrafficFormatter(GenericDataFormatter):
         """Returns fixed model parameters for experiments."""
 
         fixed_params = {
-            'total_time_steps': 10 * 24,
+            'total_time_steps': 9 * 24,
             'num_encoder_steps': 7 * 24,
             'num_epochs': 50,
             'early_stopping_patience': 5,
