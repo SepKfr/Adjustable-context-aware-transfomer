e48cdc3255303b7cc6cefa70767090067810c1b1
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-08-27 15:42:37 -0500

    creating self attention scores plots

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -416,6 +416,7 @@ def perform_evaluation(args, device, test_en, test_de, test_y, test_id, formatte
         tgt_all_input = tgt_all_input.reshape(test_en.shape[0]*test_en.shape[1], -1)
 
         ind = 0
+        diff = 0
         for i in range(15872):
             loss_attn_temp = math.sqrt(criterion(torch.from_numpy(pred_attn_temp_cutoff[i, :]),
                                                  torch.from_numpy(tgt_all[i, :])))
@@ -427,7 +428,9 @@ def perform_evaluation(args, device, test_en, test_de, test_y, test_id, formatte
                                             torch.from_numpy(tgt_all[i, :])))
             if loss_attn_temp < loss_attn and loss_attn_temp < loss_attn_conv and \
                     loss_attn_temp < loss_attn_multi:
-                ind = i
+                if loss_attn - loss_attn_temp > diff:
+                    ind = i
+                    diff = loss_attn - loss_attn_temp
 
         y_max = max(max(enc_attn_scores[ind, :]),
                     max(enc_attn_conv_scores[ind, :]),
