691f8d913151a29bc2b5d28d585d303cab072c62
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-02 15:43:13 -0400

    plot the attention scores

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -565,7 +565,6 @@ def perform_evaluation(args, device, test_en, test_de, test_y, test_id, formatte
             else ax_3.set_ylabel("Electricity Consumption") if args.exp_name == "electricity" \
             else ax_3.set_ylabel("Occupancy Rate")
 
-        fig.tight_layout()
         plt.subplots_adjust(bottom=0)
         plt.savefig(os.path.join(args.path_to_save, 'pred_plot_attn_{}_{}.png'.format(args.exp_name, len_of_pred)))
         plt.close()
