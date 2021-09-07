a15dc57eb8e128d90847476277936409423c8434
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-07 15:21:10 -0400

    adding solar

--- data/solar.py
+++ data/solar.py
@@ -29,6 +29,7 @@ class SolarFormatter(ElectricityFormatter):
         ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.TIME),
         ('day_of_week', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
         ('hour', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+        ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
         ('Power(MW)', DataTypes.REAL_VALUED, InputTypes.TARGET),
         ('id', DataTypes.REAL_VALUED, InputTypes.ID),
         ('categorical_id', DataTypes.CATEGORICAL, InputTypes.STATIC_INPUT),
