32c367a982bbb53c8f651fbef6ac0041fa9c984e
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-14 15:11:51 -0400

    change fig size

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -492,11 +492,9 @@ def perform_evaluation(args, device, test_en, test_de, test_y, test_id, formatte
                         np.sum(self_attn_temp_cutoff_scores[ind, :])
                     ))
 
+        fig, ax_1 = plt.subplots(figsize=(6, 4))
         plt.rc('axes', labelsize=14)
         plt.rc('axes', titlesize=14)
-        plt.rcParams["figure.figsize"] = (8, 6)
-
-        fig, ax_1 = plt.subplots()
 
         ax_1.plot(np.arange(-enc_step, 0), enc_attn_scores[ind, :], color='red')
         ax_1.plot(np.arange(-enc_step, 0), enc_attn_multi_scores[ind, :], color='violet')
@@ -534,10 +532,9 @@ def perform_evaluation(args, device, test_en, test_de, test_y, test_id, formatte
                     min(dec_enc_attn_temp_cutoff_scores[ind, :] /
                         np.sum(dec_enc_attn_temp_cutoff_scores[ind, :])))
 
-        fig, ax_2 = plt.subplots()
+        fig, ax_2 = plt.subplots(figsize=(6, 4))
         plt.rc('axes', labelsize=14)
         plt.rc('axes', titlesize=14)
-        plt.rcParams["figure.figsize"] = (8, 6)
 
         ax_2.plot(np.arange(-enc_step, 0), dec_enc_attn_scores[ind, :], color='red')
         ax_2.plot(np.arange(-enc_step, 0), dec_enc_attn_multi_scores[ind, :], color='violet')
@@ -569,10 +566,9 @@ def perform_evaluation(args, device, test_en, test_de, test_y, test_id, formatte
                     max(pred_attn_conv[ind, :]),
                     max(pred_attn_temp_cutoff[ind, :]))
 
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=(6, 4))
         plt.rc('axes', labelsize=14)
         plt.rc('axes', titlesize=14)
-        plt.rcParams["figure.figsize"] = (8, 6)
 
         ax.plot(np.arange(-enc_step, total_len - enc_step), np.concatenate((tgt_all_input[ind, :], tgt_all[ind, :])),
                  color='blue')
