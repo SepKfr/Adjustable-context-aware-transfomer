fbcece5288d33013ddb1350a736d9c7cc951b546
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-15 10:17:03 -0400

    add the loc of legend at best

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -466,9 +466,7 @@ def perform_evaluation(args, device, test_en, test_de, test_y, test_id, formatte
                                             torch.from_numpy(tgt_all[i, :])))
             if loss_attn_temp < loss_attn and loss_attn_temp < loss_attn_conv and \
                     loss_attn_temp < loss_attn_multi:
-                if loss_attn_multi - loss_attn_temp > diff:
-                    diff = loss_attn_multi - loss_attn_temp
-                    ind = i
+               ind = i
 
         y_max = max(max(enc_attn_scores[ind, :]),
                     max(enc_attn_conv_scores[ind, :]),
