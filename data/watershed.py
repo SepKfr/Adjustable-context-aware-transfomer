9a0035b73d00b8e176c5f4a3cf7d93b5093bb6ed
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-22 17:00:45 -0400

    trying local aware with less queries a new method called cat q reduced

--- data/watershed.py
+++ data/watershed.py
@@ -75,7 +75,7 @@ class WatershedFormatter(DataFormatter):
         """Returns fixed model parameters for experiments."""
 
         fixed_params = {
-            'total_time_steps': 9 * 24,
+            'total_time_steps': 8 * 24,
             'num_encoder_steps': 7 * 24,
             'num_epochs': 50,
             'early_stopping_patience': 5,
@@ -106,4 +106,4 @@ class WatershedFormatter(DataFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 2560, 256
+        return 128000, 16000
