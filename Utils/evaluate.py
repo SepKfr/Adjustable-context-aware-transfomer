7fb4247e505904753cdcff1c9e8980ce2529dd32
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-23 20:27:56 -0400

    make plots with subscripts

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -540,7 +540,10 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
                                             torch.from_numpy(tgt_all[i, :])))
             if loss_attn_temp < loss_attn and loss_attn_temp < loss_attn_conv and \
                     loss_attn_temp < loss_attn_multi:
-                ind = i
+                if loss_attn - loss_attn_temp > diff_1 and loss_attn_multi - loss_attn_temp > diff_2:
+                    diff_1 = loss_attn - loss_attn_temp
+                    diff_2 = loss_attn_multi - loss_attn_temp
+                    ind = i
 
         y_max = max(max(enc_attn_scores[ind, :]),
                     max(enc_attn_conv_scores[ind, :]),
