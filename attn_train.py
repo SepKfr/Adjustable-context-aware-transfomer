793ee8ad564dc6b9144b8ea70597f16971b195b0
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-27 10:54:44 -0400

    saving train_loss for further analysis

--- attn_train.py
+++ attn_train.py
@@ -91,6 +91,7 @@ def train(args, model, train_en, train_de, train_y,
                 torch.save({'model_state_dict': model.state_dict()}, os.path.join(path, args.name))
                 if epoch == num_epochs - 1:
                     torch.save({'train_loss': train_loss_list}, os.path.join(path, args.name))
+                    del train_loss_list
 
             e = epoch
 
@@ -109,7 +110,7 @@ def train(args, model, train_en, train_de, train_y,
         }, os.path.join(path, "{}_continue".format(args.name)))
         sys.exit(0)
 
-    return best_config, val_loss, val_inner_loss, stop, e, train_loss_list
+    return best_config, val_loss, val_inner_loss, stop, e
 
 
 def create_config(hyper_parameters):
@@ -302,7 +303,7 @@ def main():
 
         for epoch in range(epoch_start, params['num_epochs'], 1):
 
-            best_config, val_loss, val_inner_loss, stop, e, train_loss_list = \
+            best_config, val_loss, val_inner_loss, stop, e = \
                 train(args, model, train_en_p.to(device), train_de_p.to(device),
                       train_y_p.to(device), valid_en_p.to(device), valid_de_p.to(device),
                       valid_y_p.to(device), epoch, e, params['num_epochs'], val_loss, val_inner_loss,
