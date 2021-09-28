39c800b0fe224bc019c1bf51788554af8eb257f3
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-28 17:57:11 -0400

    plotting train loss

--- data/traffic.py
+++ data/traffic.py
@@ -170,7 +170,7 @@ class TrafficFormatter(GenericDataFormatter):
 
         model_params = {
             'dropout_rate': 0.3,
-            'hidden_layer_size': [16],
+            'hidden_layer_size': [16, 32],
             'learning_rate': 0.001,
             'minibatch_size': [256],
             'max_gradient_norm': 100.,
