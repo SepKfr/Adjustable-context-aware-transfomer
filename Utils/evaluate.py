1608e227761c5e3aad513dc82a4a0a9d1c329cca
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-28 22:10:48 -0400

    plotting index

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -743,7 +743,7 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
         index = np.where(index == 2, 9, index)
         index = np.where(index == 5, 0, index)
         fig, ax = plt.subplots(figsize=(6, 4))
-        ax.matshow(index, cmap=plt.get_cmap('hot'))
+        ax.matshow(index, cmap=plt.get_cmap('PuRd'))
         plt.tight_layout()
         plt.axis('off')
         plt.savefig(os.path.join(args.path_to_save, 'matrix_{}_{}.pdf'.format(args.exp_name, len_pred)),
