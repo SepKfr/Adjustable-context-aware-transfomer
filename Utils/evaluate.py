a73acf175f2a5f803ce1e001bd7d136ea461ad3d
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-09-28 20:38:55 -0400

    plotting index

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -734,6 +734,7 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
         output, dec_enc_index = model(test_en[ind], test_de[ind])
         index = dec_enc_index[-1, -1, :, :]
         index = index.reshape(-1, 1)
+        print(index.shape)
         index = index.detach().cpu().numpy()
         plt.matshow(index)
         plt.savefig(os.path.join(args.path_to_save, 'matrix_{}_{}.pdf'.format(args.exp_name, len_pred)),
