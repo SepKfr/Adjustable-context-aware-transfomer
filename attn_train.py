4897da1f009d3b8c89002d2c16a726d460c05a54
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-10-16 10:56:24 -0400

    add osftmax

--- attn_train.py
+++ attn_train.py
@@ -184,7 +184,7 @@ def main():
     parser.add_argument("--n_heads_best", type=int)
     parser.add_argument("--n_layers", type=int, default=1)
     parser.add_argument("--n_layers_best", type=int)
-    parser.add_argument("--kernel", type=int, default=3)
+    parser.add_argument("--kernel", type=list, default=[1, 3, 6, 9])
     parser.add_argument("--kernel_best", type=int)
     parser.add_argument("--dr", type=list, default=[0])
     parser.add_argument("--dr_best", type=float)
@@ -254,9 +254,9 @@ def main():
 
     criterion = nn.MSELoss()
     if args.attn_type != "conv_attn" and args.attn_type != "f_linear":
-        args.kernel = 1
+        args.kernel = [1]
     hyper_param = list([[args.n_layers], model_params['minibatch_size'], [model_params['num_heads']],
-                        model_params['hidden_layer_size'], [args.kernel]])
+                        model_params['hidden_layer_size'], args.kernel])
     configs = create_config(hyper_param)
     print('number of config: {}'.format(len(configs)))
 
