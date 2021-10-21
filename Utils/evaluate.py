8dab1fae69991756a68bd22240bb6b44d8e9a3dd
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-10-21 17:58:47 -0400

    plotting for the last one

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -507,25 +507,23 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
                 get_attn_scores(attn_temp_cutoff_model, tgt_all_input, tgt_all,test_de, test_en, test_id,
                         test_y_output, test_y_input, flg)
 
-        print(test_de.shape)
-
         enc_attn_scores, self_attn_scores, dec_enc_attn_scores = \
-            np.mean(enc_attn_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0] * test_de.shape[1], -1),\
-            np.mean(self_attn_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0]*test_de.shape[1], -1), \
-            np.mean(dec_enc_attn_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0]*test_de.shape[1], -1)
+            np.mean(np.mean(enc_attn_scores, axis=0), axis=-2).reshape(test_de.shape[0] * test_de.shape[1], -1),\
+            np.mean(np.mean(self_attn_scores, axis=0), axis=-2).reshape(test_de.shape[0]*test_de.shape[1], -1), \
+            np.mean(np.mean(dec_enc_attn_scores, axis=0), axis=-2).reshape(test_de.shape[0]*test_de.shape[1], -1)
         enc_attn_multi_scores, self_attn_multi_scores, dec_enc_attn_multi_scores = \
-            np.mean(enc_attn_multi_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0] * test_de.shape[1], -1), \
-            np.mean(self_attn_multi_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0]*test_de.shape[1], -1), \
-            np.mean(dec_enc_attn_multi_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0]*test_de.shape[1], -1)
+            np.mean(np.mean(enc_attn_multi_scores, axis=0), axis=-2).reshape(test_de.shape[0] * test_de.shape[1], -1), \
+            np.mean(np.mean(self_attn_multi_scores, axis=0), axis=-2).reshape(test_de.shape[0]*test_de.shape[1], -1), \
+            np.mean(np.mean(dec_enc_attn_multi_scores, axis=0), axis=-2).reshape(test_de.shape[0]*test_de.shape[1], -1)
         enc_attn_conv_scores, self_attn_conv_scores, dec_enc_attn_conv_scores = \
-            np.mean(enc_attn_conv_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0] * test_de.shape[1], -1), \
-            np.mean(self_attn_conv_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0]*test_de.shape[1], -1), \
-            np.mean(dec_enc_attn_conv_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0]*test_de.shape[1], -1)
+            np.mean(np.mean(enc_attn_conv_scores, axis=0), axis=-2).reshape(test_de.shape[0] * test_de.shape[1], -1), \
+            np.mean(np.mean(self_attn_conv_scores, axis=0), axis=-2).reshape(test_de.shape[0]*test_de.shape[1], -1), \
+            np.mean(np.mean(dec_enc_attn_conv_scores, axis=0), axis=-2).reshape(test_de.shape[0]*test_de.shape[1], -1)
         enc_attn_temp_cutoff_scores, self_attn_temp_cutoff_scores, dec_enc_attn_temp_cutoff_scores = \
-            np.mean(enc_attn_temp_cutoff_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0] * test_de.shape[1],
+            np.mean(np.mean(enc_attn_temp_cutoff_scores, axis=0), axis=-2).reshape(test_de.shape[0] * test_de.shape[1],
                                                                                     -1),\
-            np.mean(self_attn_temp_cutoff_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0]*test_de.shape[1], -1), \
-            np.mean(dec_enc_attn_temp_cutoff_scores, axis=0)[:, :, -1, :].reshape(test_de.shape[0]*test_de.shape[1], -1)
+            np.mean(np.mean(self_attn_temp_cutoff_scores, axis=0), axis=-2).reshape(test_de.shape[0]*test_de.shape[1], -1), \
+            np.mean(np.mean(dec_enc_attn_temp_cutoff_scores, axis=0), axis=-2).reshape(test_de.shape[0]*test_de.shape[1], -1)
 
         pred_attn = np.mean(predictions_attn, axis=0).reshape(test_de.shape[0]*test_de.shape[1], -1)
         pred_attn_multi = np.mean(predictions_attn_multi, axis=0).reshape(test_de.shape[0]*test_de.shape[1], -1)
@@ -581,15 +579,22 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
         fig, ax_1 = plt.subplots()
         x = np.arange(-enc_step, 0)
         x_1 = np.arange(0, total_len - enc_step)
+        xnew = np.linspace(min(x), max(x), 300)
+        spl = make_interp_spline(x, enc_attn_temp_cutoff_scores[ind,], k=3)
+        enc_self_smooth = spl(xnew)
+
+        xnew_1 = np.linspace(min(x_1), max(x_1), 300)
+        spl = make_interp_spline(x_1, self_attn_temp_cutoff_scores[ind,], k=3)
+        dec_self_smooth = spl(xnew_1)
 
         ax_1.plot(x, enc_attn_scores[ind, ], color='lightgreen')
         ax_1.plot(x, enc_attn_multi_scores[ind, ], color='plum')
         ax_1.plot(x, enc_attn_conv_scores[ind, ], color='darksalmon')
-        ax_1.plot(x, enc_attn_temp_cutoff_scores[ind, ], color='darkblue')
+        ax_1.plot(xnew, enc_self_smooth, color='darkblue')
         ax_1.plot(x_1, self_attn_scores[ind, ], color='lightgreen')
         ax_1.plot(x_1, self_attn_multi_scores[ind, ], color='plum')
         ax_1.plot(x_1, self_attn_conv_scores[ind, ], color='darksalmon')
-        ax_1.plot(x_1, self_attn_temp_cutoff_scores[ind, ], color='darkblue')
+        ax_1.plot(xnew_1, dec_self_smooth, color='darkblue')
         ax_1.vlines(0, ymin=y_min, ymax=y_max, colors='black')
         ax_1.legend(['Transformer', 'Trans-multi',
                     'CNN-trans', 'Ours'], loc="best")
@@ -627,7 +632,6 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
         ax_2.plot(x, dec_enc_attn_multi_scores[ind, ], color='plum')
         ax_2.plot(x, dec_enc_attn_conv_scores[ind, ], color='darksalmon')
         ax_2.plot(xnew, power_smooth, color='darkblue')
-        #ax_2.plot(xnew, power_smooth, color='lightblue', linestyle='--')
         ax_2.vlines(0, ymin=y_min, ymax=y_max, colors='black')
         ax_2.legend(['Transformer', 'Trans-multi',
                     'CNN-trans', 'Ours'], loc="best")
