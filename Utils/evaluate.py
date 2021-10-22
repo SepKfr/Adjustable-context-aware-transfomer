bc4a7f938d5db0f917a6e3aa78c8a1146a75abd6
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-10-22 12:58:14 -0400

    plotting filter length distribution

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -749,6 +749,7 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
         ind3 = random.randint(0, 8)
         print(ind_2)
         print(ind3)
+        print(dec_enc_index.shape)
         index = dec_enc_index[ind_2, ind3, :, :]
         index = index.detach().cpu().numpy()
         index = index[:, 0::4]
