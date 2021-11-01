51281bfb742590c989527456a0c74910ef9c4cd4
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-11-01 02:31:19 -0400

    move nn to init

--- models/attn.py
+++ models/attn.py
@@ -75,12 +75,13 @@ class ScaledDotProductAttention(nn.Module):
         self.d_k = d_k
         self.attn_type = attn_type
         self.kernel = kernel
-        self.linear_s = list()
+        self.linear_s = []
         self.filter_length = [2, 3, 6, 9]
         for f in self.filter_length:
-            self.linear_s.append(nn.Linear(f, 1).to(self.device))
-        self.w_c = nn.Parameter(torch.randn((2, len(self.filter_length) - 1))).to(self.device)
-        self.conv1d = nn.Conv1d(in_channels=d_k * h, out_channels=d_k * h, kernel_size=kernel).to(self.device)
+            self.linear_s.append(nn.Linear(f, 1))
+        self.linear_s = nn.ModuleList(self.linear_s)
+        self.w_c = nn.Parameter(torch.randn((2, len(self.filter_length) - 1)))
+        self.conv1d = nn.Conv1d(in_channels=d_k * h, out_channels=d_k * h, kernel_size=kernel)
 
     def get_conv(self, kernel, q_p, k_p):
 
