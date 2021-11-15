39dbab8f5669d1aef9ab615f559c57dbe43812b5
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-11-15 14:29:55 -0500

    solving cudnn error

--- rnn_train.py
+++ rnn_train.py
@@ -183,7 +183,7 @@ def main():
     parser.add_argument("--exp_name", type=str, default='watershed')
     parser.add_argument("--cuda", type=str, default='cuda:0')
     parser.add_argument("--seed", type=int, default=21)
-    parser.add_argument("--total_time_steps", type=int, default=216)
+    parser.add_argument("--total_time_steps", type=int, default=192)
     args = parser.parse_args()
 
     np.random.seed(21)
