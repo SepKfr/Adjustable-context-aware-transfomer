15cc4e812abd77df4dd247ddd1aa5b1dd7b6e225
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-11-21 10:44:05 -0500

    dump data to csv

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -888,7 +888,6 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
 
         id = pd.concat(df_list, axis=0).to_numpy()
         id = np.repeat(id, 48, axis=0)
-        print(id)
         data_to_dump[0, :] = tgt_all
         data_to_dump[1, :] = flow_rate_postfix
         data_to_dump[2, :] = predictions_lstm
