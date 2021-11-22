ec83012e61cf453305e570467d48b3c93dfe47ab
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-11-21 19:05:46 -0500

    dump data to csv

--- data/data_loader.py
+++ data/data_loader.py
@@ -150,6 +150,7 @@ def process_watershed(config):
 
     date = output.index
     output['day_of_week'] = date.dayofweek
+    print(date.hour)
     output['d'] = date.year*10000000000 + date.month*100000000 + date.day*1000000 + date.hour*10000 + date.minute*100 + date.second
     output['hour'] = date.hour
     output['id'] = output['Site']
