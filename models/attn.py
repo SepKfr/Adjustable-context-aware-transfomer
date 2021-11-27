3203057de2bf66ba342cf86568fff8e7c002f9e6
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-11-26 23:18:55 -0500

    add to filter length by 3

--- models/attn.py
+++ models/attn.py
@@ -75,7 +75,7 @@ class ScaledDotProductAttention(nn.Module):
         self.d_k = d_k
         self.attn_type = attn_type
         self.kernel = kernel
-        self.filter_length = [1, 3, 6, 21]
+        self.filter_length = [1, 3, 6, 24]
         self.linear_list_q = nn.ModuleList([nn.Linear(f, 1) for f in self.filter_length]).to(device)
         self.linear_list_k = nn.ModuleList([nn.Linear(f, 1) for f in self.filter_length]).to(device)
         self.w_c = nn.Linear(2, max(self.filter_length) - 1, bias=False).to(device)
