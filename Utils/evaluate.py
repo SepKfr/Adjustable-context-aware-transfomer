0bb9a80cd6934f9cd57c680fc5de76bc8a0b4772
sepidekoohfar <sepide1.koohfar@gmail.com>
2021-12-27 13:43:03 -0500

    create attn_matrix

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -796,6 +796,7 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
         index = np.where(index == 1, 3, index)
         index = np.where(index == 0, 1, index)
         index = np.where(index == 2, 6, index)
+        index = index.reshape(index.shape[1], index.shape[2])
 
         #index = np.where(index == 5, -2, index)
         norm_bins = np.sort([1, 3, 6, 9]) + 0.5
