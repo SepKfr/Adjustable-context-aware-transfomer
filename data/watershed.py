4f33659ea67217eefece3e6e0b34441b6c0d5c45
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-01-05 14:37:18 -0500

    removing two softmaxs

--- data/watershed.py
+++ data/watershed.py
@@ -124,7 +124,7 @@ class WatershedFormatter(DataFormatter):
 
         model_params = {
             'dropout_rate': 0.3,
-            'hidden_layer_size': [16],
+            'hidden_layer_size': [16, 32],
             'learning_rate': 0.001,
             'minibatch_size': [256],
             'max_gradient_norm': 100,
