b97792ccbc3549f541c2f44db402968f608b53e9
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-01-24 13:56:10 -0500

    create rmse plots

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -219,7 +219,7 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
             predictions_attn_temp_cutoff = np.zeros((3, test_de.shape[0], test_de.shape[1], timesteps))
 
             flag = False
-            for i, seed in enumerate([9, 1992]):
+            for i, seed in enumerate([21, 9, 1992]):
 
                 torch.manual_seed(seed)
                 input_size = test_en.shape[3]
