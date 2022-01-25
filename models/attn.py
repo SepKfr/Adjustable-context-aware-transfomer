a5b8293c8683213d9bf24046b8b63c3a60ffcf7f
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-01-25 13:57:22 -0500

    create rmse plots seperate

--- models/attn.py
+++ models/attn.py
@@ -430,5 +430,5 @@ class Attn(nn.Module):
         enc_outputs, enc_self_attns = self.encoder(enc_inputs)
         dec_outputs, dec_self_attns, dec_enc_attns = self.decoder(dec_inputs, enc_outputs)
         dec_logits = self.projection(dec_outputs)
-        return dec_logits
+        return dec_logits, enc_self_attns, dec_self_attns, dec_enc_attns
 
