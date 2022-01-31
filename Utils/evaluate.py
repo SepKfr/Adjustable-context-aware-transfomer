aa8f0672f249b92b94f320303e0b7ca0071b0045
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-01-31 18:47:26 -0500

    create context size matrix with x and y lable

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -635,6 +635,7 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
         loss_temp = 1e9
         diff_1 = 0
         diff_2 = 0
+        diff_3 = 0
         for i in range(15872):
             loss_attn_temp = math.sqrt(criterion(torch.from_numpy(pred_context_aware_1369[i, :]),
                                                  torch.from_numpy(tgt_all[i, :])))
@@ -650,9 +651,11 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
             if loss_attn_temp < loss_temp and loss_attn_temp < loss_attn and loss_attn_temp < loss_attn_conv and \
                     loss_attn_temp < loss_attn_multi and loss_attn_temp < loss_lstm:
                 loss_temp = loss_attn_temp
-                if loss_attn - loss_attn_temp > diff_1 and loss_lstm - loss_attn_temp > diff_2:
+                if loss_attn - loss_attn_temp > diff_1 and loss_lstm - loss_attn_temp > diff_2 \
+                        and loss_attn_conv - loss_attn_temp > diff_3:
                     diff_1 = loss_attn - loss_attn_temp
                     diff_2 = loss_lstm - loss_attn_temp
+                    diff_3 = loss_attn_conv - loss_attn_temp
                     ind = i
 
         '''y_max = max(max(enc_attn_scores[ind, :]),
