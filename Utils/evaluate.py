7085951539c4d9c182ad239f20cf1ae7e497e3b1
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-02-01 16:50:53 -0500

    generating matrix and ground truth for datasets

--- Utils/evaluate.py
+++ Utils/evaluate.py
@@ -905,8 +905,7 @@ def perform_evaluation(args, device, params, test, valid_max, formatter):
 
         y_min = min(min(tgt_all_input[ind[0], ind[1], :]), min(tgt_all[ind[0], ind[1], :]))
         y_max = max(max(tgt_all_input[ind[0], ind[1], :]), max(tgt_all[ind[0], ind[1], :]))
-        plt.scatter(np.append(np.arange(-168, 0, 9), 0), np.append(tgt_all_input[ind[0], ind[1], 0::9],
-                                                                   tgt_all_input[ind[0], ind[1], -1]), color='darkviolet')
+        plt.scatter(np.arange(-168, 0, 9), tgt_all_input[ind[0], ind[1], 0::9], color='darkviolet')
         plt.scatter(np.arange(0, 48, 3), tgt_all[ind[0], ind[1], 0::3], color='darkorange')
         plt.plot(np.arange(-168, total_len - 168), np.concatenate((tgt_all_input[ind[0], ind[1], :], tgt_all[ind[0], ind[1], :])),
                 color='blue')
