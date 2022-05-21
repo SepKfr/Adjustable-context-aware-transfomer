d383169b9e49590090dd7041ca23ecb84545567b
anonymous-1992 <33998911+anonymous-1992@users.noreply.github.com>
2022-05-21 12:48:36 -0400

    Delete run.sh
    deleting run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-for dataset in watershed electricity traffic
-do
-  for seed in 21 9 1992
-  do
-    python train.py --seed $seed --exp_name $dataset
-    python test.py --seed $seed --exp_name $dataset
-  done
-done
