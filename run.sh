448b2d4a23ccc1664988eb06e92ffed24e456723
anonymous-1992 <33998911+anonymous-1992@users.noreply.github.com>
2022-05-21 12:30:38 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 for dataset in watershed electricity traffic
 do
-  for seed in 21, 9, 1992
+  for seed in 21 9 1992
   do
     python train.py --seed $seed --exp_name $dataset
     python test.py --seed $seed --exp_name $dataset
