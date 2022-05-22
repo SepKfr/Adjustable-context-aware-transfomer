34a38fb5c8858d05628a39e10d4199d5e127b132
sepidekoohfar <sepide1.koohfar@gmail.com>
2022-05-22 08:32:14 -0400

    revert

--- Utils/base_train.py
+++ Utils/base_train.py
@@ -12,32 +12,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # Lint as: python3
+
 import torch
 import numpy as np
 from Utils import utils, base
 import pandas as pd
-import random
 InputTypes = base.InputTypes
 
 
 def batching(batch_size, x_en, x_de, y_t, test_id):
+
     batch_n = int(x_en.shape[0] / batch_size)
     start = x_en.shape[0] % batch_n
     X_en = torch.zeros(batch_n, batch_size, x_en.shape[1], x_en.shape[2])
     X_de = torch.zeros(batch_n, batch_size, x_de.shape[1], x_de.shape[2])
     Y_t = torch.zeros(batch_n, batch_size, y_t.shape[1], y_t.shape[2])
     tst_id = np.empty((batch_n, batch_size, test_id.shape[1], x_en.shape[2]), dtype=object)
+
     for i in range(batch_n):
         X_en[i, :, :, :] = x_en[start:start+batch_size, :, :]
         X_de[i, :, :, :] = x_de[start:start+batch_size, :, :]
         Y_t[i, :, :, :] = y_t[start:start+batch_size, :, :]
         tst_id[i, :, :, :] = test_id[start:start+batch_size, :, :]
         start += batch_size
+
     return X_en, X_de, Y_t, tst_id
 
 
-def batch_sampled_data(data, max_samples, time_steps, num_encoder_steps, column_definition, seed):
+def batch_sampled_data(data, max_samples, time_steps, num_encoder_steps, column_definition):
     """Samples segments into a compatible format.
     Args:
       data: Sources data to sample and batch
@@ -46,15 +50,15 @@ def batch_sampled_data(data, max_samples, time_steps, num_encoder_steps, column_
       Dictionary of batched data with the maximum samples specified.
     """
 
-    np.random.seed(seed)
-    random.seed(seed)
-
     if max_samples < 1:
         raise ValueError(
-            'Illegal number of samples specified! samples={}'.format(max_samples))
+          'Illegal number of samples specified! samples={}'.format(max_samples))
+
     id_col = utils.get_single_col_by_input_type(InputTypes.ID, column_definition)
     time_col = utils.get_single_col_by_input_type(InputTypes.TIME, column_definition)
+
     data.sort_values(by=[id_col, time_col], inplace=True)
+
     valid_sampling_locations = []
     split_data_map = {}
     for identifier, df in data.groupby(id_col):
@@ -64,16 +68,19 @@ def batch_sampled_data(data, max_samples, time_steps, num_encoder_steps, column_
                 (identifier, time_steps + i)
                 for i in range(num_entries - time_steps + 1)
             ]
+
             split_data_map[identifier] = df
+
     if 0 < max_samples < len(valid_sampling_locations):
         ranges = [
-            valid_sampling_locations[i] for i in np.random.choice(
-                len(valid_sampling_locations), max_samples, replace=False)
+          valid_sampling_locations[i] for i in np.random.choice(
+              len(valid_sampling_locations), max_samples, replace=False)
         ]
     else:
         print('Max samples={} exceeds # available segments={}'.format(
-            max_samples, len(valid_sampling_locations)))
+          max_samples, len(valid_sampling_locations)))
         ranges = valid_sampling_locations
+
     id_col = utils.get_single_col_by_input_type(InputTypes.ID, column_definition)
     time_col = utils.get_single_col_by_input_type(InputTypes.TIME, column_definition)
     target_col = utils.get_single_col_by_input_type(InputTypes.TARGET, column_definition)
@@ -94,18 +101,20 @@ def batch_sampled_data(data, max_samples, time_steps, num_encoder_steps, column_
     outputs = np.zeros((max_samples, time_steps, 1))
     time = np.empty((max_samples, time_steps, 1), dtype=object)
     identifiers = np.empty((max_samples, time_steps, 1), dtype=object)
+
     for i, tup in enumerate(ranges):
         if (i + 1 % 1000) == 0:
             print(i + 1, 'of', max_samples, 'samples done...')
         identifier, start_idx = tup
         sliced = split_data_map[identifier].iloc[start_idx -
-                                                 time_steps:start_idx]
+                                               time_steps:start_idx]
         enc_inputs[i, :, :] = sliced[enc_input_cols].iloc[:num_encoder_steps]
         dec_inputs[i, :, :] = sliced[dec_input_cols].iloc[num_encoder_steps:]
         inputs[i, :, :] = sliced[enc_input_cols]
         outputs[i, :, :] = sliced[[target_col]]
         time[i, :, 0] = sliced[time_col]
         identifiers[i, :, 0] = sliced[id_col]
+
     sampled_data = {
         'inputs': inputs,
         'enc_inputs': enc_inputs,
@@ -115,9 +124,12 @@ def batch_sampled_data(data, max_samples, time_steps, num_encoder_steps, column_
         'time': time,
         'identifier': identifiers
     }
+
     return sampled_data
 
+
 def inverse_output(predictions, outputs, test_id):
+
     def format_outputs(preds):
         flat_prediction = pd.DataFrame(
             preds[:, :, 0],
--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-import math
-
 from models.context_aware_attn import Attn
 from torch.optim import Adam
 import torch.nn as nn
@@ -13,98 +11,47 @@ import sys
 import random
 import pandas as pd
 from data.data_loader import ExperimentConfig
-from Utils.base_train import batching, batch_sampled_data, inverse_output
+from Utils.base_train import batching, batch_sampled_data
 
 
 class NoamOpt:
+
     def __init__(self, optimizer, lr_mul, d_model, n_warmup_steps):
         self._optimizer = optimizer
         self.lr_mul = lr_mul
         self.d_model = d_model
         self.n_warmup_steps = n_warmup_steps
         self.n_steps = 0
+
     def step_and_update_lr(self):
         "Step with the inner optimizer"
         self._update_learning_rate()
         self._optimizer.step()
-    def zero_grad(self):
 
+    def zero_grad(self):
         "Zero out the gradients with the inner optimizer"
         self._optimizer.zero_grad()
-    def _get_lr_scale(self):
 
+    def _get_lr_scale(self):
         d_model = self.d_model
         n_steps, n_warmup_steps = self.n_steps, self.n_warmup_steps
         return (d_model ** -0.5) * min(n_steps ** (-0.5), n_steps * n_warmup_steps ** (-1.5))
 
     def _update_learning_rate(self):
-
         ''' Learning rate scheduling per step '''
+
         self.n_steps += 1
         lr = self.lr_mul * self._get_lr_scale()
+
         for param_group in self._optimizer.param_groups:
             param_group['lr'] = lr
-
-
-def evaluate(d_model, args, test_en, test_de, test_y, test_id, formatter, path, device):
-
-    n_heads = 8
-    d_k = int(d_model / n_heads)
-    mae = nn.L1Loss()
-    mse = nn.MSELoss()
-    model_params = formatter.get_default_model_params()
-
-    def extract_numerical_data(data):
-        """Strips out forecast time and identifier columns."""
-        return data[[
-            col for col in data.columns
-            if col not in {"forecast_time", "identifier"}
-        ]]
-
-    model = Attn(src_input_size=test_en.shape[3],
-                 tgt_input_size=test_de.shape[3],
-                 d_model=d_model,
-                 d_ff=d_model * 4,
-                 d_k=d_k, d_v=d_k, n_heads=n_heads,
-                 n_layers=model_params['stack_size'], src_pad_index=0,
-                 tgt_pad_index=0, device=device,
-                 context_lengths=model_params['context_lengths'],
-                 attn_type=args.attn_type, seed=args.seed).to(device)
-
-    checkpoint = torch.load(os.path.join(path, "{}_{}".format(args.name, args.seed)))
-    model.load_state_dict(checkpoint["model_state_dict"])
-
-    model.eval()
-
-    predictions = torch.zeros(test_y.shape[0], test_y.shape[1], test_y.shape[2])
-    targets_all = torch.zeros(test_y.shape[0], test_y.shape[1], test_y.shape[2])
-
-    for j in range(test_en.shape[0]):
-        output = model(test_en[j], test_de[j])
-        output_map = inverse_output(output, test_y[j], test_id[j])
-        forecast = torch.from_numpy(extract_numerical_data(
-            formatter.format_predictions(output_map["predictions"])).to_numpy().astype('float32')).to(device)
-
-        predictions[j, :, :] = forecast
-        targets = torch.from_numpy(extract_numerical_data(
-            formatter.format_predictions(output_map["targets"])).to_numpy().astype('float32')).to(device)
-
-        targets_all[j, :, :] = targets
-
-    normaliser = targets_all.to(device).abs().mean()
-    test_loss = mse(predictions.to(device), targets_all.to(device)).item()
-    test_loss = math.sqrt(test_loss) / normaliser.item()
-
-    mae_loss = mae(predictions.to(device), targets_all.to(device)).item()
-    mae_loss = mae_loss / normaliser.item()
-
-    return test_loss, mae_loss
 
 
 def train(args, model, train_en, train_de, train_y,
           test_en, test_de, test_y, epoch, e, val_loss,
           val_inner_loss, optimizer, train_loss_list,
           config, config_num, best_config, criterion, path):
+
     stop = False
     try:
         model.train()
@@ -117,23 +64,30 @@ def train(args, model, train_en, train_de, train_y,
             optimizer.zero_grad()
             loss.backward()
             optimizer.step_and_update_lr()
+
         print("Train epoch: {}, loss: {:.4f}".format(epoch, total_loss))
+
         model.eval()
         test_loss = 0
         for j in range(test_en.shape[0]):
             outputs = model(test_en[j], test_de[j])
             loss = criterion(test_y[j], outputs)
             test_loss += loss.item()
+
         if test_loss < val_inner_loss:
             val_inner_loss = test_loss
             if val_inner_loss < val_loss:
                 val_loss = val_inner_loss
                 best_config = config
-                torch.save({'model_state_dict': model.state_dict()}, os.path.join(path, "{}_{}".format(args.name, str(args.seed))))
+                torch.save({'model_state_dict': model.state_dict()}, os.path.join(path, args.name))
+
             e = epoch
+
         if epoch - e > 5:
             stop = True
+
         print("Average loss: {:.4f}".format(test_loss))
+
     except KeyboardInterrupt:
         torch.save({
             'epoch': epoch,
@@ -142,13 +96,14 @@ def train(args, model, train_en, train_de, train_y,
             'best_config': best_config
         }, os.path.join(path, "{}_continue".format(args.name)))
         sys.exit(0)
+
     return best_config, val_loss, val_inner_loss, stop, e
 
 
 def create_config(hyper_parameters):
-
     prod = list(itertools.product(*hyper_parameters))
     return list(random.sample(set(prod), len(prod)))
+
 
 def main():
 
@@ -157,132 +112,124 @@ def main():
     parser.add_argument("--exp_name", type=str, default='electricity')
     parser.add_argument("--seed", type=int, default=21)
     parser.add_argument("--cuda", type=str, default='cuda:0')
-    parser.add_argument("--attn_type", type=str, default='basic')
-    parser.add_argument("--total_steps", type=int, default=192)
     args = parser.parse_args()
     config_file = dict()
-    np.random.seed(args.seed)
-    random.seed(args.seed)
+
+    np.random.seed(21)
+    random.seed(21)
+
     torch.manual_seed(args.seed)
+
     device = torch.device(args.cuda if torch.cuda.is_available() else "cpu")
     if torch.cuda.is_available():
         print("Running on GPU")
+
     config = ExperimentConfig(args.exp_name)
     formatter = config.make_data_formatter()
+
     data_csv_path = "{}.csv".format(args.exp_name)
+
     print("Loading & splitting data...")
-    raw_data = pd.read_csv(data_csv_path)
+    raw_data = pd.read_csv(data_csv_path, index_col=0)
     train_data, valid, test = formatter.split_data(raw_data)
     train_max, valid_max = formatter.get_num_samples_for_calibration()
     params = formatter.get_experiment_params()
+
     sample_data = batch_sampled_data(train_data, train_max, params['total_time_steps'],
-                       params['num_encoder_steps'], params["column_definition"], args.seed)
+                       params['num_encoder_steps'], params["column_definition"])
     train_en, train_de, train_y, train_id = torch.from_numpy(sample_data['enc_inputs']).to(device), \
                                             torch.from_numpy(sample_data['dec_inputs']).to(device), \
                                  torch.from_numpy(sample_data['outputs']).to(device), \
                                  sample_data['identifier']
+
     sample_data = batch_sampled_data(valid, valid_max, params['total_time_steps'],
-                                     params['num_encoder_steps'], params["column_definition"], args.seed)
+                                     params['num_encoder_steps'], params["column_definition"])
     valid_en, valid_de, valid_y, valid_id = torch.from_numpy(sample_data['enc_inputs']).to(device), \
                                             torch.from_numpy(sample_data['dec_inputs']).to(device), \
                                  torch.from_numpy(sample_data['outputs']).to(device), \
                                  sample_data['identifier']
 
-    sample_data = batch_sampled_data(test, valid_max, params['total_time_steps'],
-                                     params['num_encoder_steps'], params["column_definition"], args.seed)
-    test_en, test_de, test_y, test_id = torch.from_numpy(sample_data['enc_inputs']).to(device), \
-                                            torch.from_numpy(sample_data['dec_inputs']).to(device), \
-                                            torch.from_numpy(sample_data['outputs']).to(device), \
-                                            sample_data['identifier']
-
     model_params = formatter.get_default_model_params()
+
     seq_len = params['total_time_steps'] - params['num_encoder_steps']
     path = "models_{}_{}".format(args.exp_name, seq_len)
     if not os.path.exists(path):
         os.makedirs(path)
+
     criterion = nn.MSELoss()
-    configs = list(model_params['hidden_layer_size'])
+    hyper_param = list([model_params['minibatch_size'], [model_params['num_heads']],
+                        model_params['hidden_layer_size'], args.filter_lengths])
+    configs = create_config(hyper_param)
     print('number of config: {}'.format(len(configs)))
+
     val_loss = 1e10
     best_config = configs[0]
     config_num = 0
-    batch_size = 256
+
     for i, conf in enumerate(configs, config_num):
         print('config {}: {}'.format(i+1, conf))
-        d_model = conf
-        n_heads = model_params['num_heads']
+
+        batch_size, n_heads, d_model = conf
         d_k = int(d_model / n_heads)
+
         train_en_p, train_de_p, train_y_p, train_id_p = batching(batch_size, train_en,
                                                          train_de, train_y, train_id)
+
         valid_en_p, valid_de_p, valid_y_p, valid_id_p = batching(batch_size, valid_en,
                                                          valid_de, valid_y, valid_id)
+
         model = Attn(src_input_size=train_en_p.shape[3],
                      tgt_input_size=train_de_p.shape[3],
                      d_model=d_model,
                      d_ff=d_model*4,
                      d_k=d_k, d_v=d_k, n_heads=n_heads,
-                     n_layers=model_params['stack_size'], src_pad_index=0,
+                     n_layers=params['stack_size'], src_pad_index=0,
                      tgt_pad_index=0, device=device,
-                     context_lengths=model_params['context_lengths'],
-                     attn_type=args.attn_type, seed=args.seed)
+                     context_lengths=model_params['context_lengths'])
         model.to(device)
+
         optim = NoamOpt(Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9), 2, d_model, 4000)
+
         epoch_start = 0
+
         val_inner_loss = 1e10
         train_loss_list = list()
+
         for epoch in range(epoch_start, params['num_epochs'], 1):
+
             best_config, val_loss, val_inner_loss, stop, e = \
                 train(args, model, train_en_p.to(device), train_de_p.to(device),
                       train_y_p.to(device), valid_en_p.to(device), valid_de_p.to(device),
                       valid_y_p.to(device), epoch, params['num_epochs'], val_loss, val_inner_loss,
                       optim, train_loss_list, conf, i, best_config, criterion, path)
+
             if stop:
                 break
 
-    d_model = best_config
-    test_en, test_de, test_y, test_id = batching(batch_size, test_en,
-                                                             test_de, test_y, test_id)
-    nrmse, nmae = evaluate(d_model, args, test_en.to(device),
-                           test_de.to(device), test_y.to(device), test_id, formatter, path, device)
-
+    batch_size, heads, d_model = best_config
     print("best_config: {}".format(best_config))
+
     config_file[args.name] = list()
+    config_file[args.name].append(batch_size)
+    config_file[args.name].append(heads)
     config_file[args.name].append(d_model)
+
     config_path = "configs_{}_{}.json".format(args.exp_name, seq_len)
+
     if os.path.exists(config_path):
         with open(config_path) as json_file:
             json_dat = json.load(json_file)
             if json_dat.get(args.name) is None:
                 json_dat[args.name] = list()
+            json_dat[args.name].append(batch_size)
+            json_dat[args.name].append(heads)
             json_dat[args.name].append(d_model)
+
         with open(config_path, "w") as json_file:
             json.dump(json_dat, json_file)
     else:
         with open(config_path, "w") as json_file:
             json.dump(config_file, json_file)
-
-    error_file = dict()
-    key = "{}_{}".format(args.name, args.seed)
-    error_file[key] = list()
-    error_file[key].append("{:.3f}".format(nrmse))
-    error_file[key].append("{:.3f}".format(nmae))
-
-    res_path = "results_{}_{}.json".format(args.exp_name,
-                                           args.total_steps - params['num_encoder_steps'])
-
-    if os.path.exists(res_path):
-        with open(res_path) as json_file:
-            json_dat = json.load(json_file)
-            if json_dat.get(key) is None:
-                json_dat[key] = list()
-            json_dat[key].append("{:.3f}".format(nrmse))
-            json_dat[key].append("{:.3f}".format(nmae))
-
-        with open(res_path, "w") as json_file:
-            json.dump(json_dat, json_file)
-    else:
-        with open(res_path, "w") as json_file:
-            json.dump(error_file, json_file)
 
 
 if __name__ == '__main__':
