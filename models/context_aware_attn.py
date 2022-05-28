409a732fa12538660da2a4aa95c01c1df290f6ac
sepidehkoohfar <sepide1.koohfar@gamil.com>
2022-05-28 13:48:01 -0400

    adding informer

--- models/context_aware_attn.py
+++ models/context_aware_attn.py
@@ -70,6 +70,8 @@ class ProbAttention(nn.Module):
         K_expand = K.unsqueeze(-3).expand(B, H, L_Q, L_K, E)
         index_sample = torch.randint(L_K, (L_Q, sample_k))  # real U = U_part(factor*ln(L_k))*L_q
         K_sample = K_expand[:, :, torch.arange(L_Q).unsqueeze(1), index_sample, :]
+        print(Q.shape)
+        print(K.shape)
         Q_K_sample = torch.matmul(Q.unsqueeze(-2), K_sample.transpose(-2, -1)).squeeze(-2)
 
         # find the Top_k query with sparisty measurement
