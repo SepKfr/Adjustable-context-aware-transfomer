4a980a0366fb3c71d62cb5f5f030aa50302316a7
sepidehkoohfar <sepide1.koohfar@gamil.com>
2022-05-28 13:30:49 -0400

    adding informer

--- models/context_aware_attn.py
+++ models/context_aware_attn.py
@@ -288,7 +288,7 @@ class MultiHeadAttention(nn.Module):
             context, attn = ConvAttn(d_k=self.d_k, device=self.device, kernel=self.kernel, h=self.n_heads)(
                 Q=q_s, K=k_s, V=v_s, attn_mask=attn_mask)
         else:
-            context, attn = ProbAttention()(Q=q_s, K=k_s, V=v_s, attn_mask=attn_mask)
+            context, attn = ProbAttention()(q_s, k_s, v_s, attn_mask)
         context = context.transpose(1, 2).contiguous().view(batch_size, -1, self.n_heads * self.d_v)
         output = self.fc(context)
         return output, attn
